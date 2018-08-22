885e6820e622d150dc3defe8d756cf750543f70a
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-08-22 23:47:11 +0530

    trime whitespace from python file

--- imageDownload.py
+++ imageDownload.py
@@ -1,17 +1,17 @@
 # imported the requests library
 import requests
 image_url = input()
- 
+
 # URL of the image to be downloaded is defined as image_url
 r = requests.get(image_url) # create HTTP response object
- 
+
 # send a HTTP request to the server and save
 # the HTTP response in a response object called r
+
 with open("python_logo.png",'wb') as f:
- 
     # Saving received content as a png file in
     # binary format
- 
+
     # write the contents of the response (r.content)
     # to a new file in binary mode.
     f.write(r.content)
