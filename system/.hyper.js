296cd65ef6bb89ad39ec7371af8459334cc5b805
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2020-03-30 02:49:00 +0530

    Modified dotfiles: hyper config changed
    - Font Family changed to Monaco

--- system/.hyper.js
+++ system/.hyper.js
@@ -13,7 +13,7 @@ module.exports = {
 
     // font family with optional fallbacks
     fontFamily:
-      'Operator Mono Lig Book, "Source Code Pro",  Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      'Monaco, Operator Mono Lig Book, "Source Code Pro",  Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: "bold",
@@ -89,7 +89,7 @@ module.exports = {
       lightBlue: "#6A76FB",
       lightMagenta: "#FD7CFC",
       lightCyan: "#68FDFE",
-      lightWhite: "#FFFFFF"
+      lightWhite: "#FFFFFF",
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -136,7 +136,7 @@ module.exports = {
 
     // Whether to use the WebGL renderer. Set it to false to use canvas-based
     // rendering (slower, but supports transparent backgrounds)
-    webGLRenderer: true
+    webGLRenderer: true,
 
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
@@ -151,7 +151,7 @@ module.exports = {
     "hyper-match",
     "hyper-tab-icons",
     "shades-of-purple-hyper",
-    "hyper-tabs-enhanced"
+    "hyper-tabs-enhanced",
   ],
 
   // in development, you can create a directory under
@@ -162,5 +162,5 @@ module.exports = {
   keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
-  }
+  },
 };
