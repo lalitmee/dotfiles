af521386875377e6feb8fc69a4b88f6dc1d10640
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-01-08 19:19:25 +0530

    Add emacs profiles config

---
+++ system/.emacs-profiles.el
@@ -0,0 +1,5 @@
+; (("default"   . ((user-emacs-directory . "~/doom-emacs")))
+;  ("spacemacs"   . ((user-emacs-directory . "~/spacemacs"))))
+
+(("default"   . ((user-emacs-directory . "~/spacemacs")))
+ ("doom-emacs"   . ((user-emacs-directory . "~/doom-emacs"))))
