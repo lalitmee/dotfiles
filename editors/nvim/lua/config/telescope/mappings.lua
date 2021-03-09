13307666efebefc82022cc14d6bc1e86393401ca
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-03-10 04:03:44 +0530

    fix telescope key bindings

--- editors/nvim/lua/config/telescope/mappings.lua
+++ editors/nvim/lua/config/telescope/mappings.lua
@@ -11,17 +11,13 @@ local map_tele = function(key, f, options, buffer)
 
   TelescopeMapArgs[map_key] = options or {}
 
-  local mode = "n"
+  local mode = 'n'
   local rhs = string.format(
-    "<cmd>lua R('config.telescope')['%s'](TelescopeMapArgs['%s'])<CR>",
-    f,
-    map_key
-  )
+                  '<cmd>lua R(\'config.telescope\')[\'%s\'](TelescopeMapArgs[\'%s\'])<CR>',
+                  f, map_key
+              )
 
-  local map_options = {
-    noremap = true,
-    silent = true,
-  }
+  local map_options = { noremap = true, silent = true }
 
   if not buffer then
     vim.api.nvim_set_keymap(mode, key, rhs, map_options)
@@ -30,30 +26,37 @@ local map_tele = function(key, f, options, buffer)
   end
 end
 
-vim.api.nvim_set_keymap('c', '<c-r><c-r>', '<Plug>(TelescopeFuzzyCommandSearch)', { noremap = false, nowait = true })
+vim.api.nvim_set_keymap(
+    'c', '<c-r><c-r>', '<Plug>(TelescopeFuzzyCommandSearch)',
+    { noremap = false, nowait = true }
+)
 
 -- Dotfiles
 map_tele('<leader>ofn', 'edit_neovim')
 map_tele('<leader>ofc', 'edit_dotfiles')
 
 -- Search
-map_tele('<space>osw', 'grep_string', {
-  short_path = true,
-  word_match = '-w',
-  only_sort_text = true,
-  -- layout_strategy = 'vertical',
-  sorter = sorters.get_fzy_sorter(),
-})
-map_tele('<space>osr', 'grep_last_search', {
-  -- layout_strategy = 'vertical',
-})
+map_tele(
+    '<space>osw', 'grep_string', {
+      short_path = true,
+      word_match = '-w',
+      only_sort_text = true,
+      -- layout_strategy = 'vertical',
+      sorter = sorters.get_fzy_sorter()
+    }
+)
+map_tele(
+    '<space>osr', 'grep_last_search', {
+      -- layout_strategy = 'vertical',
+    }
+)
 
 -- -- Files
 -- map_tele('<space>ft', 'git_files')
 -- map_tele('<space>fg', 'live_grep')
 map_tele('<space>ofl', 'oldfiles')
 -- map_tele('<space>fd', 'fd')
--- map_tele('<space>pp', 'project_search')
+-- map_tele('<space>pf', 'project_search')
 -- map_tele('<space>fv', 'find_nvim_source')
 -- map_tele('<space>fe', 'file_browser')
 
