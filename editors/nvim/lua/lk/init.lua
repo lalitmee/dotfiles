c4dfd333fd79e1fe546734ba811e6be5421a7e3a
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-03-31 19:29:42 +0530

    fix neogit and change plugins require

--- editors/nvim/lua/lk/init.lua
+++ editors/nvim/lua/lk/init.lua
@@ -1,16 +1,16 @@
 vim.g.mapleader = ' ' -- Remap leader key
 vim.g.maplocalleader = ',' -- Local leader is ,
 
-require('lk.utils')
-require('lk.globals')
-require('lk.settings')
-require('lk.highlights')
-require('lk.folds')
-require('lk.numbers')
-require('lk.options')
-require('lk.functions')
-require('lk.mappings')
+require('lk/utils')
+require('lk/globals')
+require('lk/settings')
+require('lk/highlights')
+require('lk/folds')
+require('lk/numbers')
+require('lk/options')
+require('lk/functions')
+require('lk/mappings')
 
-require('lk.colorscheme')
-require('lk.statusline')
-require('lk.plugins')
+require('lk/colorscheme')
+require('lk/statusline')
+require('lk/plugins')
