a07cb7e32c62b7a8c32deef041631389701141c9
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-04-01 22:37:44 +0530

    add mapping of <CR> for indentation using compe

--- editors/nvim/lua/lk/mappings.lua
+++ editors/nvim/lua/lk/mappings.lua
@@ -100,3 +100,26 @@ map('n', '<A-q>', [[:lopen<CR>]], opts)
 map('n', '<CR>', [[@=(foldlevel('.')?'za':"\<Space>")<CR>]], opts)
 -- create folds using visual select and then press <CR>
 map('v', '<CR>', [[zf]], opts)
+
+-- NOTE: auto-pairs mapping <CR> for indentation inside html files
+local npairs = require('nvim-autopairs')
+
+vim.g.completion_confirm_key = ''
+lk_utils.completion_confirm = function()
+  if vim.fn.pumvisible() ~= 0 then
+    if vim.fn.complete_info()['selected'] ~= -1 then
+      vim.fn['compe#confirm']()
+      return npairs.esc('<c-y>')
+    else
+      vim.defer_fn(function()
+        vim.fn['compe#confirm']('<cr>')
+      end, 20)
+      return npairs.esc('<c-n>')
+    end
+  else
+    return npairs.check_break_line_char()
+  end
+end
+
+map('i', '<CR>', 'v:lua.lk_utils.completion_confirm()',
+    { expr = true, noremap = true })
