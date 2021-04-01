d2b94af4aebef2d2d98c60980c17a526cc0b9345
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-04-01 14:46:51 +0530

    remove options file

--- editors/nvim/lua/lk/plugins/devicons.lua
+++ editors/nvim/lua/lk/plugins/devicons.lua
@@ -1,1 +1,3 @@
-require'nvim-web-devicons'.setup { get_icons = require('nvim-nonicons') }
+local nvim_web_icons = require('nvim-web-devicons')
+
+nvim_web_icons.setup { get_icons = require('nvim-nonicons') }
