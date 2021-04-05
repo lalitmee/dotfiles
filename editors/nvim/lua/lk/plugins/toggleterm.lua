f8cf2e63adf5b2e43a2758bd928f4107c3ea013f
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-04-05 10:33:18 +0530

    add commands to toggleterm config

--- editors/nvim/lua/lk/plugins/toggleterm.lua
+++ editors/nvim/lua/lk/plugins/toggleterm.lua
@@ -9,5 +9,6 @@ require'toggleterm'.setup {
 }
 
 -- commands
-vim.cmd [[command! -count=1 TermGitPush lua require'toggleterm'.exec("git push", <count>, 12)]]
-vim.cmd [[command! -count=1 TermGitPushF lua require'toggleterm'.exec("git push -f", <count>, 12)]]
+vim.cmd [[command! -count=1 TermGitPushSet lua require'toggleterm'.exec("gpsup", <count>, 12)]]
+vim.cmd [[command! -count=1 TermGitPush lua require'toggleterm'.exec("gp", <count>, 12)]]
+vim.cmd [[command! -count=1 TermGitPushF lua require'toggleterm'.exec("gpf", <count>, 12)]]
