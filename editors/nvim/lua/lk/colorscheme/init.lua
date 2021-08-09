cd37bcfda874f36e2ffca4a913dd73c1f1ed086b
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-08-09 14:41:24 +0530

    change(nvim): change colorscheme to onedark

--- editors/nvim/lua/lk/colorscheme/init.lua
+++ editors/nvim/lua/lk/colorscheme/init.lua
@@ -1,8 +1,8 @@
--- NOTE: vim-code-dark
-require('lk/colorscheme/vscode')
-
 -- -- NOTE: gruvqueen
 -- require('lk/colorscheme/gruvqueen')
+
+-- -- NOTE: vim-code-dark
+-- require('lk/colorscheme/vscode')
 
 -- -- NOTE: calvera
 -- require('lk/colorscheme/calvera')
@@ -10,8 +10,8 @@ require('lk/colorscheme/vscode')
 -- -- NOTE: github
 -- require('lk/colorscheme/github')
 
--- -- NOTE: onedark
--- require('lk/colorscheme/onedark')
+-- NOTE: onedark
+require('lk/colorscheme/onedark')
 
 -- -- NOTE: gruvbuddy
 -- require('lk/colorscheme/gruvbuddy')
