0a23f32a37d171535d24c7ab6c1e2dc140fc494e
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-08-26 17:34:26 +0530

    change(nvim): make spacemacs default

--- system/.emacs-profiles.el
+++ system/.emacs-profiles.el
@@ -1,7 +1,7 @@
 ; ;; for doom-emacs as default
-(("default"   . ((user-emacs-directory . "~/doom-emacs")))
-  ("spacemacs"   . ((user-emacs-directory . "~/spacemacs"))))
+;; (("default"   . ((user-emacs-directory . "~/doom-emacs")))
+;;   ("spacemacs"   . ((user-emacs-directory . "~/spacemacs"))))
 
 ;; for spacemacs as default
-; (("default"   . ((user-emacs-directory . "~/spacemacs")))
-;  ("doom-emacs"   . ((user-emacs-directory . "~/doom-emacs"))))
+(("default"   . ((user-emacs-directory . "~/spacemacs")))
+  ("doom-emacs"   . ((user-emacs-directory . "~/doom-emacs"))))
