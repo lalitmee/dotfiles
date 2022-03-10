aa83b1cc65af580f5553da0a10fbf1a9cb203250
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-10 19:30:20 +0530

    feat(neoclip): fix neoclip not working properly

--- nvim/.config/nvim/after/plugin/neoclip.lua
+++ nvim/.config/nvim/after/plugin/neoclip.lua
@@ -1,9 +1,13 @@
 require("neoclip").setup({
   enable_persistent_history = true,
+  enable_macro_history = true,
+  preview = true,
   keys = {
     telescope = {
-      i = { select = "<c-p>", paste = "<CR>", paste_behind = "<c-k>" },
+      i = { select = "<c-p>", paste = "<CR>", paste_behind = "<c-s-p>" },
       n = { select = "p", paste = "<CR>", paste_behind = "P" },
     },
   },
 })
+
+require("telescope").load_extension("neoclip")
