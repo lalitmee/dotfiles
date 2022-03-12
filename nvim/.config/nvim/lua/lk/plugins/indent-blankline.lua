5d2dcc2b1e5e4d289f51048dc2798976feee5efa
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-12 19:57:01 +0530

    fix(listchar): remove eol listchar

--- nvim/.config/nvim/lua/lk/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/lk/plugins/indent-blankline.lua
@@ -1,9 +1,6 @@
-vim.opt.listchars:append("eol:↴")
-
 require("indent_blankline").setup({
-  -- show_end_of_line = true,
   space_char_blankline = " ",
-  char = "│", -- ┆ ┊ 
+  char = "┆", -- │ ┊ 
   show_foldtext = false,
   show_current_context = true,
   show_current_context_start = false,
