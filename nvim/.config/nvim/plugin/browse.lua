89a7762f60c93b97eaf2b3ed223d414140084101
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-12 23:40:24 +0530

    feat(browse): improve Browse command

--- nvim/.config/nvim/plugin/browse.lua
+++ nvim/.config/nvim/plugin/browse.lua
@@ -1,4 +1,4 @@
-function Browse()
+local function input_search()
   vim.ui.input({ prompt = "Search String: " }, function(input)
     if input == nil then
       return
@@ -6,3 +6,14 @@ function Browse()
     vim.fn.jobstart(string.format("xdg-open 'https://www.google.com/search?q=%s'", input))
   end)
 end
+
+function Browse(args)
+  if args["args"] == "" then
+    input_search()
+    return
+  end
+  local search_string = args["args"]
+  vim.fn.jobstart(string.format("xdg-open 'https://www.google.com/search?q=%s'", search_string))
+end
+
+vim.api.nvim_add_user_command("Browse", Browse, { nargs = "*" })
