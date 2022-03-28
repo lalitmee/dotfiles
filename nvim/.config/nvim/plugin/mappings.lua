4d41e6d12e350bfdca43ec6bd97ff884989857cf
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-28 17:05:03 +0530

    refactor(mappings.lua): use comment boxes

--- nvim/.config/nvim/plugin/mappings.lua
+++ nvim/.config/nvim/plugin/mappings.lua
@@ -4,11 +4,30 @@ local nnoremap = lk.nnoremap
 local map_opts = { noremap = true, silent = true }
 local map_expr_opts = { expr = true }
 
-nnoremap("<Esc><Esc>", ":<C-u>nohlsearch<cr>")
-
+----------------------------------------------------------------------
+-- NOTE: movements {{{
+----------------------------------------------------------------------
 nnoremap("0", "^")
 nnoremap("^", "0")
 
+-- behave vim
+map("n", "Y", [[y$]], map_opts)
+map("n", "H", [[0]], map_opts)
+map("n", "L", [[$]], map_opts)
+
+-- jumplist mutations
+map("n", "j", [[(v:count > 5 ? "m'" . v:count : "") . 'j']], { noremap = true, expr = true })
+map("n", "k", [[(v:count > 5 ? "m'" . v:count : "") . 'k']], { noremap = true, expr = true })
+
+-- <c-n> and <c-p> in command line mode
+map("c", "<C-n>", [[wildmenumode() ? "\<c-n>" : "\<down>"]], map_expr_opts)
+map("c", "<C-p>", [[wildmenumode() ? "\<c-p>" : "\<up>"]], map_expr_opts)
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: buffers and tabs {{{
+----------------------------------------------------------------------
 -- tab operations
 nnoremap("<M-Right>", [[:tabnext<CR>]])
 nnoremap("<M-Left>", [[:tabprevious<CR>]])
@@ -26,18 +45,26 @@ nnoremap("<C-Left>", [[:bp<cr>]])
 -- alternate file mapping
 map("n", "<bs>", [[:<c-u>exe v:count ? v:count . 'b' : 'b' . (bufloaded(0) ? '#' : 'n')<cr>]], map_opts)
 
+-- NOTE: this was creating problem with <C-I>
+-- circular window movements
+-- map('n', '<c-tab>', [[<C-w>w]], map_opts)
+-- map('n', '<s-tab>', [[<C-w>W]])
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: copy and paste {{{
+----------------------------------------------------------------------
 -- copy and pasting made easy
 map("n", "<localleader>y", [["+y]], { noremap = true })
 map("n", "<localleader>p", [["+p]], { noremap = true })
+-- }}}
+----------------------------------------------------------------------
 
--- behave vim
-map("n", "Y", [[y$]], map_opts)
-map("n", "H", [[0]], map_opts)
-map("n", "L", [[$]], map_opts)
-
--- keeping it centered
-map("n", "n", [[nzzzv]], map_opts)
-map("n", "N", [[Nzzzv]], map_opts)
+----------------------------------------------------------------------
+-- NOTE: text edits {{{
+----------------------------------------------------------------------
+-- keep cursor in same position while using `J`
 map("n", "J", [[:let p=getpos('.')<bar>join<bar>call setpos('.', p)<cr>]], map_opts)
 
 -- undo breakpoints
@@ -45,10 +72,6 @@ map("i", ",", [[,<c-g>u]], map_opts)
 map("i", ".", [[.<c-g>u]], map_opts)
 map("i", "!", [[!<c-g>u]], map_opts)
 map("i", "?", [[?<c-g>u]], map_opts)
-
--- jumplist mutations
-map("n", "j", [[(v:count > 5 ? "m'" . v:count : "") . 'j']], { noremap = true, expr = true })
-map("n", "k", [[(v:count > 5 ? "m'" . v:count : "") . 'k']], { noremap = true, expr = true })
 
 -- moving text up and down
 map("i", "<C-k>", [[<esc>:m .-2<cr>==]], map_opts)
@@ -60,68 +83,106 @@ map("v", "K", [[:m '<-2<cr>gv=gv]], map_opts)
 map("n", "cn", [[*``cgn]], map_opts)
 map("n", "cN", [[*``cgN]], map_opts)
 
--- -- count number of lines in visual mode
--- map('v', 'L', [[g<C-g>]], map_opts)
+-- Allow saving of files as sudo when I forgot to start vim using sudo.
+-- http://stackoverflow.com/questions/2600783/how-does-the-vim-write-with-sudo-trick-work
+map("c", "w!!", [[%!sudo tee > /dev/null %]], map_opts)
+-- }}}
+----------------------------------------------------------------------
 
--- NOTE: this was creating problem with <C-I>
--- circular window movements
--- map('n', '<c-tab>', [[<C-w>w]], map_opts)
--- map('n', '<s-tab>', [[<C-w>W]])
+----------------------------------------------------------------------
+-- NOTE: search {{{
+----------------------------------------------------------------------
+-- keeping it centered
+map("n", "n", [[nzzzv]], map_opts)
+map("n", "N", [[Nzzzv]], map_opts)
+
+-- clear search pressing ESC two times
+nnoremap("<Esc><Esc>", ":<C-u>nohlsearch<cr>")
 
 -- go to search and replace mode
 map("n", "<C-s>", [[:%s/]], map_opts)
 
--- <c-n> and <c-p> in command line mode
-map("c", "<C-n>", [[wildmenumode() ? "\<c-n>" : "\<down>"]], map_expr_opts)
-map("c", "<C-p>", [[wildmenumode() ? "\<c-p>" : "\<up>"]], map_expr_opts)
-
--- <c-l> for syntax highlight and more
-nnoremap("<c-l>", [[:nohlsearch<cr>:diffupdate<cr>:syntax sync fromstart<cr><c-l>]])
-
--- Allow saving of files as sudo when I forgot to start vim using sudo.
--- http://stackoverflow.com/questions/2600783/how-does-the-vim-write-with-sudo-trick-work
-map("c", "w!!", [[%!sudo tee > /dev/null %]], map_opts)
-
 -- clear highlighted search
 map("n", "<cr>", [[:noh<cr>]], map_opts)
-
--- resize panes
-map("n", "<Right>", [[:vertical resize +5<cr>]], map_opts)
-map("n", "<Left>", [[:vertical resize -5<cr>]], map_opts)
-map("n", "<Up>", [[:resize +5<cr>]], map_opts)
-map("n", "<Down>", [[:resize -5<cr>]], map_opts)
-
--- Visually select the text that was last edited/pasted
-map("n", "gV", [[`[v`]], map_opts)
-
--- I hate escape more than anything else
-map("i", "jk", [[<Esc>]], map_opts)
-map("i", "kj", [[<Esc>]], map_opts)
-
--- " Easy CAPS
--- map('n', '<c-u>', [[<ESC>viwUi]], map_opts)
--- map('n', '<c-l>', [[viwl<Esc>]], map_opts)
 
 -- repeat `n.` after editing the searched word
 map("n", "Q", [[@='n.'<cr>]], map_opts)
 
+-- <c-l> for syntax highlight and more
+nnoremap("<c-l>", [[:nohlsearch<cr>:diffupdate<cr>:syntax sync fromstart<cr><c-l>]])
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: resize panes {{{
+----------------------------------------------------------------------
+map("n", "<Right>", [[:vertical resize +5<cr>]], map_opts)
+map("n", "<Left>", [[:vertical resize -5<cr>]], map_opts)
+map("n", "<Up>", [[:resize +5<cr>]], map_opts)
+map("n", "<Down>", [[:resize -5<cr>]], map_opts)
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: selection {{{
+----------------------------------------------------------------------
+-- Visually select the text that was last edited/pasted
+map("n", "gV", [[`[v`]], map_opts)
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: escape using `jk` or `kj` {{{
+-- handy in some use cases where pressing escape closes the window
+----------------------------------------------------------------------
+map("i", "jk", [[<Esc>]], map_opts)
+map("i", "kj", [[<Esc>]], map_opts)
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: terminal {{{
+----------------------------------------------------------------------
 -- pasting from registers in terminal
 map("t", "<expr>", [[<A-r> '<C-\><C-N>"'.nr2char(getchar()).'pi']], map_opts)
+-- }}}
+----------------------------------------------------------------------
 
--- NOTE: Transpose characters xp {{{
+----------------------------------------------------------------------
+-- NOTE: transpose characters `xp` {{{
 -- picked from http://vimcasts.org/episodes/creating-repeatable-mappings-with-repeat-vim/
+----------------------------------------------------------------------
 map("n", "<Plug>TransposeCharacters xp", [[:call repeat#set("\<Plug>TransposeCharacters")<cr>]], map_opts)
 map("n", "cp", [[<Plug>TransposeCharacters]])
-
 -- }}}
+----------------------------------------------------------------------
 
--- NOTE: folds mappings
+----------------------------------------------------------------------
+-- NOTE: folds {{{
+----------------------------------------------------------------------
 -- if there is a fold under cursor open it by pressing <cr> otherwise do
 -- what <cr> does
 map("n", "<cr>", [[@=(foldlevel('.')?'za':"\<Space>")<cr>]], map_opts)
 -- create folds using visual select and then press <cr>
 map("v", "<cr>", [[zf]], map_opts)
+-- }}}
+----------------------------------------------------------------------
 
+----------------------------------------------------------------------
+-- NOTE: shortcuts {{{
+----------------------------------------------------------------------
+-- -- count number of lines in visual mode
+-- map('v', 'L', [[g<C-g>]], map_opts)
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: external {{{
+----------------------------------------------------------------------
 -- -- Complextras.nvim configuration
 -- map("i", "<C-x><C-m>", [[<c-r>=luaeval("require('complextras').complete_matching_line()")<cr>]], map_opts)
 -- map("i", "<C-x><C-d>", [[<c-r>=luaeval("require('complextras').complete_line_from_cwd()")<cr>]], map_opts)
+-- }}}
+----------------------------------------------------------------------
+
+-- vim:foldmethod=marker
