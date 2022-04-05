27966e8c0c0c9738ce3adfe99ca5ab9cd1fb2ec4
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-04-05 16:09:11 +0530

    feat(cursorline): autocmd for cursorline
    disable cursorline in `TelescopePrompt`

--- nvim/.config/nvim/lua/lk/core/augroups.lua
+++ nvim/.config/nvim/lua/lk/core/augroups.lua
@@ -128,4 +128,23 @@ augroup("help_tab_au", {
 -- }}}
 ----------------------------------------------------------------------
 
+----------------------------------------------------------------------
+-- NOTE: cursor line {{{
+----------------------------------------------------------------------
+local group = vim.api.nvim_create_augroup("cursor_line_au", { clear = true })
+local set_cursorline = function(event, value, pattern)
+  vim.api.nvim_create_autocmd(event, {
+    group = group,
+    pattern = pattern,
+    callback = function()
+      vim.opt_local.cursorline = value
+    end,
+  })
+end
+set_cursorline("WinLeave", false)
+set_cursorline("WinEnter", true)
+set_cursorline("FileType", false, "TelescopePrompt")
+-- }}}
+----------------------------------------------------------------------
+
 -- vim:foldmethod=marker
