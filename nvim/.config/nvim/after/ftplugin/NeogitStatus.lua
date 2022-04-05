893dfb91816ddd881bece71ba60d5fc8c4067162
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-04-05 12:49:24 +0530

    refactor(ftplugin): `NeogitStatus.lua`

---
+++ nvim/.config/nvim/after/ftplugin/NeogitStatus.lua
@@ -0,0 +1,7 @@
+local opt = vim.opt
+
+opt.fillchars = {
+  eob = [[\]], -- suppress ~ at EndOfBuffer
+}
+
+opt.foldenable = false
--- nvim/.config/nvim/after/ftplugin/NeogitStatus.vim
+++
@@ -1,4 +0,0 @@
-setlocal fillchars+=eob:\
-set formatoptions-=o
-
-autocmd FileType NeogitStatus setlocal sw=2 sts=2 ts=2 et
