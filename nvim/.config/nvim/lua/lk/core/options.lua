58bb6e6a6776c06ae76cf5610d6dc8c0a704cca4
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-06-11 11:56:14 +0530

    fix(winbar): change winbar

--- nvim/.config/nvim/lua/lk/core/options.lua
+++ nvim/.config/nvim/lua/lk/core/options.lua
@@ -424,8 +424,37 @@ vim.cmd([[
   set t_ZH=[3m
   set t_ZR=[23m
   set t_ut=
-  set winbar=%=%m\ %f
+  " set winbar=%=%m\ %f
 ]])
+-- }}}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+-- NOTE: winbar {{{
+----------------------------------------------------------------------
+function WinBar()
+  local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":p")
+  local cwd = string.gsub(vim.loop.cwd(), "([^%w])", "%%%1") -- escape non-word characters
+  path = path:gsub(cwd, ".")
+  path = path:gsub(os.getenv("HOME"), "~")
+  local elems = vim.split(path, "/", { trimempty = true })
+  return "%#WinBarPath#" .. table.concat(elems, " %#WinBarSep# %#WinBarPath#") .. " %#WinBar#%m"
+end
+
+vim.opt.winbar = ""
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function()
+    local buf = tonumber(vim.fn.expand("<abuf>"))
+    local winbar = ""
+    if vim.api.nvim_buf_get_option(buf, "buftype") == "" then
+      winbar = "%!v:lua.WinBar()"
+    end
+    local win = vim.fn.bufwinid(buf)
+    vim.api.nvim_win_set_option(win, "winbar", winbar)
+  end,
+})
 -- }}}
 ----------------------------------------------------------------------
 
--- nvim/.config/nvim/lua/lk/plugins/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lk/plugins/lsp/handlers.lua
@@ -110,21 +110,24 @@ lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_hel
 --------------------------------------------------------------------------------
 -- NOTE: show message handler {{{
 --------------------------------------------------------------------------------
-lsp.handlers["window/showMessage"] = function(_, result, ctx)
-  local client = lsp.get_client_by_id(ctx.client_id)
-  local lvl = ({
-    "ERROR",
-    "WARN",
-    "INFO",
-    "DEBUG",
-  })[result.type]
-  vim.notify({ result.message }, lvl, {
-    title = "[LSP] | " .. client.name,
-    timeout = 10000,
-    keep = function()
-      return lvl == "ERROR" or lvl == "WARN"
-    end,
-  })
+-- lsp.handlers["window/showMessage"] = function(_, result, ctx)
+--   local client = lsp.get_client_by_id(ctx.client_id)
+--   local lvl = ({
+--     "ERROR",
+--     "WARN",
+--     "INFO",
+--     "DEBUG",
+--   })[result.type]
+--   vim.notify({ result.message }, lvl, {
+--     title = "[LSP] | " .. client.name,
+--     timeout = 10000,
+--     keep = function()
+--       return lvl == "ERROR" or lvl == "WARN"
+--     end,
+--   })
+-- end
+vim.lsp.handlers["window/showMessage"] = function(_, method, params, client_id)
+  vim.notify(method.message, severity[params.type], { title = vim.lsp.get_client_by_id(client_id).name })
 end
 -- }}}
 --------------------------------------------------------------------------------
