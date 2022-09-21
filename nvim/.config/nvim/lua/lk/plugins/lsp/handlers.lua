63a5f318afa1d7bb4b792d8cb2d527d2b45fe36b
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-09-21 09:57:46 +0530

    fix(diagnostics): fix diagnostics float display

--- nvim/.config/nvim/lua/lk/plugins/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lk/plugins/lsp/handlers.lua
@@ -36,14 +36,24 @@ vim.diagnostic.config({
   -- virtual_text = false,
   signs = true,
 
-  -- options for floating windows:
   float = {
     show_header = true,
     border = "rounded",
-    source = "always",
+    -- source = "always",
     format = function(d)
+      if not d.code and not d.user_data then
+        return d.message
+      end
+
       local t = vim.deepcopy(d)
-      local code = d.code or d.user_data.lsp.code
+      local code = d.code
+      if not code then
+        if not d.user_data.lsp then
+          return d.message
+        end
+
+        code = d.user_data.lsp.code
+      end
       if code then
         t.message = string.format("%s [%s]", t.message, code):gsub("1. ", "")
       end
