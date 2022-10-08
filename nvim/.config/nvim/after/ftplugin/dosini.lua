27a4198f6788781ad8739ed15071e01740c9b0af
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-08 22:00:23 +0530

    fix(polybar): change some things in the polybar
    using icons instead of text

---
+++ nvim/.config/nvim/after/ftplugin/dosini.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt
+
+----------------------------------------------------------------------
+-- NOTE: options {{{
+----------------------------------------------------------------------
+opt.colorcolumn = { "80" }
+opt.expandtab = true
+opt.formatoptions = opt.formatoptions - "o" -- don't continue comments on `o` and `O`
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.textwidth = 80
+opt.foldlevel = 99
+-- }}}
+----------------------------------------------------------------------
