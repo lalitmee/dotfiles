3636d0ff53a325e2b6f8f749e595d4edd602e425
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-11 17:42:27 +0530

    feat(sxhkd): using `flameshot` for `screenshots`

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,6 @@ polybar-msg cmd quit
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar mainbar-i3 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar mainbar2-i3 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
