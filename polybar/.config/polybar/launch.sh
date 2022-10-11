abcc1bff7e195cbd54636704d2e95a9e22fe68d4
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-12 03:02:44 +0530

    refactor(polybar): add general format bg

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,7 +8,7 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mainbar-i3 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar mainbar 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
