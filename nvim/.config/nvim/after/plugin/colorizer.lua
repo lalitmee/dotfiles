0632252d55a8efb43bd22c517c7de1a6922271bf
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-13 14:08:18 +0530

    fix(colorizer): don't enable `colorizer` default

--- nvim/.config/nvim/after/plugin/colorizer.lua
+++ nvim/.config/nvim/after/plugin/colorizer.lua
@@ -1,6 +1,8 @@
-require("colorizer").setup({
-    filetype = { "*" },
-    user_default_options = {
-        mode = "background", -- Set the display mode.
-    },
-})
+-- NOTE: commenting out this because it was enabling colorizer by default for
+-- all the buffers and we don't want that
+-- require("colorizer").setup({
+--     -- filetype = { "*" },
+--     user_default_options = {
+--         mode = "background", -- Set the display mode.
+--     },
+-- })
