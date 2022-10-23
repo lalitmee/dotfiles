32055abbe249bc8bbb970dcd84257050b019ccf9
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-23 13:31:35 +0530

    refactor(matchit): disable `matchit`

--- nvim/.config/nvim/lua/lk/disable_builtin.lua
+++ nvim/.config/nvim/lua/lk/disable_builtin.lua
@@ -1,24 +1,24 @@
 local disabled_built_ins = {
-  "2html_plugin",
-  "getscript",
-  "getscriptPlugin",
-  "gzip",
-  "logipat",
-  -- "matchit",
-  "netrw",
-  "netrwFileHandlers",
-  "netrwPlugin",
-  "netrwSettings",
-  "rrhelper",
-  "spellfile_plugin",
-  "tar",
-  "tarPlugin",
-  "vimball",
-  "vimballPlugin",
-  "zip",
-  "zipPlugin",
+    "2html_plugin",
+    "getscript",
+    "getscriptPlugin",
+    "gzip",
+    "logipat",
+    "matchit",
+    "netrw",
+    "netrwFileHandlers",
+    "netrwPlugin",
+    "netrwSettings",
+    "rrhelper",
+    "spellfile_plugin",
+    "tar",
+    "tarPlugin",
+    "vimball",
+    "vimballPlugin",
+    "zip",
+    "zipPlugin",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-  vim.g["loaded_" .. plugin] = 1
+    vim.g["loaded_" .. plugin] = 1
 end
