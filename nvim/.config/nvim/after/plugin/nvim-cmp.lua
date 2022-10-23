f09f9dd583f1c113a8adfbf9a8411b2df7cc234c
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-24 01:16:01 +0530

    fix(nvim-cmp): `tab` in `nvim-cmp`

--- nvim/.config/nvim/after/plugin/nvim-cmp.lua
+++ nvim/.config/nvim/after/plugin/nvim-cmp.lua
@@ -12,6 +12,26 @@ vim.opt.shortmess:append("c")
 ----------------------------------------------------------------------
 -- NOTE: cmp setup {{{
 ----------------------------------------------------------------------
+local function tab(fallback)
+    if cmp.visible() then
+        cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    else
+        fallback()
+    end
+end
+
+local function s_tab(fallback)
+    if cmp.visible() then
+        cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    else
+        fallback()
+    end
+end
+
 cmp.setup({
     mapping = {
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
@@ -23,6 +43,8 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         }),
+        ["<Tab>"] = cmp.mapping({ i = tab }),
+        ["<S-Tab>"] = cmp.mapping({ i = s_tab }),
     },
     sources = cmp.config.sources({
         { name = "cmp_tabnine" },
