65542766502afb8527b636e013fe7c08d7a94ea9
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-11-01 19:43:51 +0530

    refactor(mappings): change `session` mappings

--- nvim/.config/nvim/lua/lk/plugins/sessions.lua
+++ nvim/.config/nvim/lua/lk/plugins/sessions.lua
@@ -16,9 +16,10 @@ telescope.load_extension("session-lens")
 local actions = require("telescope.actions")
 
 require("session-lens").setup({
-  theme_conf = {
-    mappings = {
-      i = { ["<esc>"] = actions.close },
+    path_display = { "shorten" },
+    theme_conf = {
+        mappings = {
+            i = { ["<esc>"] = actions.close },
+        },
     },
-  },
 })
