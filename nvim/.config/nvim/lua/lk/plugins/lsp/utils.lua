5025eefbe37f4fdfef57258c20118d51ba4a70f5
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-11-03 12:50:52 +0530

    refactor(lsp): use `LspDefinition`
    not using `Telescope lsp_definition` now

--- nvim/.config/nvim/lua/lk/plugins/lsp/utils.lua
+++ nvim/.config/nvim/lua/lk/plugins/lsp/utils.lua
@@ -37,7 +37,10 @@ M.mappings = function()
     local imap = lk.imap
 
     -- lsp mapping for the client
-    nmap("gd", "<cmd>Telescope lsp_definitions<CR>", map_opts)
+    -- NOTE: using this somehow goes into telescope with two definitions so not
+    -- using this for now until I figure out why it was happening
+    -- nmap("gd", "<cmd>Telescope lsp_definitions<CR>", map_opts)
+    nmap("gd", "<cmd>LspDefinition<CR>", map_opts)
     nmap("ge", "<cmd>Telescope diagnostics<CR>", map_opts)
     nmap("gr", "<cmd>Telescope lsp_references<CR>", map_opts)
     nmap("gw", "<cmd>Telescope lsp_document_symbols<CR>", map_opts)
