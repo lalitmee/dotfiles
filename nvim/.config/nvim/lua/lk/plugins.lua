e7f3dee862cb7512636b0bcaae6dc09087cc4f6c
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-12-23 01:36:15 +0530

    fix(pqf): add `url` parameter for `gitlab` package

--- nvim/.config/nvim/lua/lk/plugins.lua
+++ nvim/.config/nvim/lua/lk/plugins.lua
@@ -46,8 +46,7 @@ return {
     },
     { "godlygeek/tabular", cmd = { "Tabularize" } },
     {
-        "https://gitlab.com/yorickpeterse/nvim-pqf.git",
-        enabled = false,
+        url = "https://gitlab.com/yorickpeterse/nvim-pqf.git",
         config = function()
             require("pqf").setup()
         end,
