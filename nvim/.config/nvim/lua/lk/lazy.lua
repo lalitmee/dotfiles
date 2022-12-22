a6a01de53fe1969d64c2fe00ee77fac47d8a8066
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-12-22 17:05:17 +0530

    fix(which-key): add `Lazy` keymaps

--- nvim/.config/nvim/lua/lk/lazy.lua
+++ nvim/.config/nvim/lua/lk/lazy.lua
@@ -1,36 +1,30 @@
-local use_dev = false
-
-if use_dev then
-    -- use the local project
-    vim.opt.runtimepath:prepend(vim.fn.expand("~/projects/lazy.nvim"))
-else
-    -- bootstrap from github
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "--single-branch",
-            "https://github.com/folke/lazy.nvim.git",
-            lazypath,
-        })
-    end
-    vim.opt.runtimepath:prepend(lazypath)
+-- bootstrap from github
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--single-branch",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
+vim.opt.runtimepath:prepend(lazypath)
 
 -- load lazy
 require("lazy").setup("lk.plugins", {
     -- defaults = { lazy = true },
-    -- dev = { patterns = jit.os:find("Windows") and {} or { "folke" } },
-    -- install = { colorscheme = { "tokyonight", "habamax" } },
-    checker = { enabled = true },
+    dev = {
+        path = "~/Desktop/Github",
+    },
+    checker = {
+        enabled = true,
+    },
     performance = {
         rtp = {
             disabled_plugins = {
                 "gzip",
-                -- "matchit",
-                -- "matchparen",
                 "netrwPlugin",
                 "tarPlugin",
                 "tohtml",
@@ -40,5 +34,7 @@ require("lazy").setup("lk.plugins", {
         },
     },
     debug = false,
+    ui = {
+        border = "rounded",
+    },
 })
--- vim.keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>")
