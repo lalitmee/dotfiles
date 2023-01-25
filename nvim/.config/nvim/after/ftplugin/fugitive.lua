0fcb717062b3294e0c3d470d2316e670d18fcd31
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-01-25 19:40:44 +0530

    fix(fugitive): add keymaps for push and pull

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -4,4 +4,28 @@ lk.augroup("fugitive_au", {
         buffer = 0,
         command = [[ :wincmd K | resize 30]],
     },
+    {
+        event = { "BufWinEnter" },
+        buffer = 0,
+        command = function()
+            if vim.bo.ft ~= "fugitive" then
+                return
+            end
+
+            local bufnr = vim.api.nvim_get_current_buf()
+            local opts = { buffer = bufnr, remap = false }
+            vim.keymap.set("n", "<leader>p", function()
+                vim.cmd.Git("push")
+            end, opts)
+
+            -- rebase always
+            vim.keymap.set("n", "<leader>P", function()
+                vim.cmd.Git({ "pull", "--rebase" })
+            end, opts)
+
+            -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+            -- needed if i did not set the branch up correctly
+            vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+        end,
+    },
 })
