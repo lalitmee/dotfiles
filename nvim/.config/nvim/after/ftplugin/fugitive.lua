9b04a362e6271418a1ee34c9976d1105a858caad
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-01-25 19:45:02 +0530

    fix(fugitive): fix pull command

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -20,7 +20,8 @@ lk.augroup("fugitive_au", {
 
             -- rebase always
             vim.keymap.set("n", "<leader>P", function()
-                vim.cmd.Git({ "pull", "--rebase" })
+                -- vim.cmd.Git({ "pull", "--rebase" })
+                vim.cmd([[Git pull --rebase]])
             end, opts)
 
             -- NOTE: It allows me to easily set the branch i am pushing and any tracking
