8c11e2c801eaf10d68a7ed2e136f437d902beeff
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-01-25 18:06:07 +0530

    fix(fugitive): change window size

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -2,6 +2,6 @@ lk.augroup("fugitive_au", {
     {
         event = { "BufWinEnter" },
         buffer = 0,
-        command = [[ :wincmd K | resize 40]],
+        command = [[ :wincmd K | resize 30]],
     },
 })
