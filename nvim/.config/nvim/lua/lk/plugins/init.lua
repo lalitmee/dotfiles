2ddf99279dc1168fdadfe2a2961436c4c8ea8444
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-04-11 16:57:03 +0530

    refactor(colors): remove `semanticToken` highlight

--- nvim/.config/nvim/lua/lk/plugins/init.lua
+++ nvim/.config/nvim/lua/lk/plugins/init.lua
@@ -234,7 +234,7 @@ return {
 
             vim.cmd.colorscheme("catppuccin")
         end,
-        enabled = true,
+        -- enabled = false,
     },
     {
         dir = "~/Desktop/Github/cobalt2.nvim",
@@ -242,34 +242,6 @@ return {
         dependencies = { "tjdevries/colorbuddy.nvim" },
         init = function()
             require("colorbuddy").colorscheme("cobalt2")
-            local links = {
-                ["@lsp.type.namespace"] = "@namespace",
-                ["@lsp.type.type"] = "@type",
-                ["@lsp.type.class"] = "@type",
-                ["@lsp.type.enum"] = "@type",
-                ["@lsp.type.interface"] = "@type",
-                ["@lsp.type.struct"] = "@structure",
-                ["@lsp.type.parameter"] = "@parameter",
-                ["@lsp.type.variable"] = "@variable",
-                ["@lsp.type.property"] = "@property",
-                ["@lsp.type.enumMember"] = "@constant",
-                ["@lsp.type.function"] = "@function",
-                ["@lsp.type.method"] = "@method",
-                ["@lsp.type.macro"] = "@macro",
-                ["@lsp.type.decorator"] = "@function",
-            }
-            for newgroup, oldgroup in pairs(links) do
-                vim.api.nvim_set_hl(0, newgroup, {
-                    link = oldgroup,
-                    default = true,
-                })
-            end
-
-            -- -- disable semantic highlighting
-            -- for _, group in ipairs(
-            -- vim.fn.getcompletion("@lsp", "highlight")) do
-            --     vim.api.nvim_set_hl(0, group, {})
-            -- end
         end,
         dev = true,
         enabled = false,
