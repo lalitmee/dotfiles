4d6fed7c2216ccef864f7191c93a0f42fe3dbc9d
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-04-19 18:02:00 +0530

    refactor(diagnostic): disable `virtual_text`

--- nvim/.config/nvim/lua/lk/plugins/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lk/plugins/lsp/handlers.lua
@@ -9,7 +9,11 @@ local diagnostic = vim.diagnostic
 --------------------------------------------------------------------------------
 lsp.handlers["textDocument/definition"] = function(_, result)
     if not result or vim.tbl_isempty(result) then
-        vim.notify("[LSP] Could not find definition", 2, { title = "[LSP] definition" })
+        vim.notify(
+            "[LSP] Could not find definition",
+            2,
+            { title = "[LSP] definition" }
+        )
         return
     end
 
@@ -27,14 +31,14 @@ end
 --------------------------------------------------------------------------------
 vim.diagnostic.config({
     underline = true,
-    virtual_text = {
-        severity = nil,
-        source = "if_many",
-        format = function(d)
-            return d.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-        end,
-    },
-    -- virtual_text = false,
+    -- virtual_text = {
+    --     severity = nil,
+    --     source = "if_many",
+    --     format = function(d)
+    --         return d.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+    --     end,
+    -- },
+    virtual_text = false,
     signs = true,
 
     float = {
@@ -56,7 +60,8 @@ vim.diagnostic.config({
                 code = diag.user_data.lsp.code
             end
             if code then
-                t.message = string.format("%s [%s]", t.message, code):gsub("1. ", "")
+                t.message =
+                    string.format("%s [%s]", t.message, code):gsub("1. ", "")
             end
             return t.message
         end,
@@ -143,7 +148,10 @@ do
             or api.nvim_create_autocmd("DiagnosticChanged", {
                 callback = function()
                     -- skip QF lists that we did not populate
-                    if not lk.is_vim_list_open() or fn.getqflist({ title = 0 }).title ~= TITLE then
+                    if
+                        not lk.is_vim_list_open()
+                        or fn.getqflist({ title = 0 }).title ~= TITLE
+                    then
                         return
                     end
                     diagnostic.setqflist({ open = false, title = TITLE })
@@ -177,11 +185,12 @@ lsp.handlers["textDocument/hover"] = lsp.with(
 --------------------------------------------------------------------------------
 -- NOTE: signature help {{{
 --------------------------------------------------------------------------------
-lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, {
-    border = "rounded",
-    max_width = max_width,
-    max_height = max_height,
-})
+lsp.handlers["textDocument/signatureHelp"] =
+    lsp.with(lsp.handlers.signature_help, {
+        border = "rounded",
+        max_width = max_width,
+        max_height = max_height,
+    })
 -- }}}
 --------------------------------------------------------------------------------
 
@@ -208,24 +217,34 @@ end
 local implementation = function()
     local params = lsp.util.make_position_params(nil, "utf-8")
 
-    lsp.buf_request(0, "textDocument/implementation", params, function(err, result, ctx, config)
-        local bufnr = ctx.bufnr
-        local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+    lsp.buf_request(
+        0,
+        "textDocument/implementation",
+        params,
+        function(err, result, ctx, config)
+            local bufnr = ctx.bufnr
+            local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
 
-        -- In go code, I do not like to see any mocks for impls
-        if ft == "go" then
-            local new_result = vim.tbl_filter(function(v)
-                return not string.find(v.uri, "mock_")
-            end, result)
+            -- In go code, I do not like to see any mocks for impls
+            if ft == "go" then
+                local new_result = vim.tbl_filter(function(v)
+                    return not string.find(v.uri, "mock_")
+                end, result)
 
-            if #new_result > 0 then
-                result = new_result
+                if #new_result > 0 then
+                    result = new_result
+                end
             end
-        end
 
-        lsp.handlers["textDocument/implementation"](err, result, ctx, config)
-        vim.cmd([[normal! zz]])
-    end)
+            lsp.handlers["textDocument/implementation"](
+                err,
+                result,
+                ctx,
+                config
+            )
+            vim.cmd([[normal! zz]])
+        end
+    )
 end
 lsp.handlers["textDocument/implementation"] = implementation
 -- }}}
@@ -245,57 +264,86 @@ local function qf_rename()
     local position_params = vim.lsp.util.make_position_params(nil, "utf-8")
     position_params.oldName = vim.fn.expand("<cword>")
 
-    vim.ui.input({ prompt = "Rename To: ", default = position_params.oldName }, function(input)
-        if input == nil then
-            vim.notify("[LSP] aborted rename", 3, { render = "minimal" })
-            return
-        end
-
-        position_params.newName = input
-        vim.lsp.buf_request(0, "textDocument/rename", position_params, function(err, result, ...)
-            if not result or not result.changes then
-                vim.notify(string.format("could not perform rename"), 4, {
-                    title = string.format("[LSP] rename: %s -> %s", position_params.oldName, position_params.newName),
-                    timeout = 500,
-                })
+    vim.ui.input(
+        { prompt = "Rename To: ", default = position_params.oldName },
+        function(input)
+            if input == nil then
+                vim.notify("[LSP] aborted rename", 3, { render = "minimal" })
                 return
             end
 
-            vim.lsp.handlers["textDocument/rename"](err, result, ...)
+            position_params.newName = input
+            vim.lsp.buf_request(
+                0,
+                "textDocument/rename",
+                position_params,
+                function(err, result, ...)
+                    if not result or not result.changes then
+                        vim.notify(string.format("could not perform rename"), 4, {
+                            title = string.format(
+                                "[LSP] rename: %s -> %s",
+                                position_params.oldName,
+                                position_params.newName
+                            ),
+                            timeout = 500,
+                        })
+                        return
+                    end
 
-            local notification, entries = "", {}
-            local num_files, num_updates = 0, 0
-            for uri, edits in pairs(result.changes) do
-                num_files = num_files + 1
-                local bufnr = vim.uri_to_bufnr(uri)
+                    vim.lsp.handlers["textDocument/rename"](err, result, ...)
 
-                for _, edit in ipairs(edits) do
-                    local start_line = edit.range.start.line + 1
-                    local line = vim.api.nvim_buf_get_lines(bufnr, start_line - 1, start_line, false)[1]
+                    local notification, entries = "", {}
+                    local num_files, num_updates = 0, 0
+                    for uri, edits in pairs(result.changes) do
+                        num_files = num_files + 1
+                        local bufnr = vim.uri_to_bufnr(uri)
 
-                    num_updates = num_updates + 1
-                    table.insert(entries, {
-                        bufnr = bufnr,
-                        lnum = start_line,
-                        col = edit.range.start.character + 1,
-                        text = line,
+                        for _, edit in ipairs(edits) do
+                            local start_line = edit.range.start.line + 1
+                            local line = vim.api.nvim_buf_get_lines(
+                                bufnr,
+                                start_line - 1,
+                                start_line,
+                                false
+                            )[1]
+
+                            num_updates = num_updates + 1
+                            table.insert(entries, {
+                                bufnr = bufnr,
+                                lnum = start_line,
+                                col = edit.range.start.character + 1,
+                                text = line,
+                            })
+                        end
+
+                        local short_uri = string.sub(
+                            vim.uri_to_fname(uri),
+                            #vim.fn.getcwd() + 2
+                        )
+                        notification = notification
+                            .. string.format(
+                                "made %d change(s) in %s",
+                                #edits,
+                                short_uri
+                            )
+                    end
+
+                    vim.notify(notification, 2, {
+                        title = string.format(
+                            "[LSP] rename: %s -> %s",
+                            position_params.oldName,
+                            position_params.newName
+                        ),
+                        timeout = 2500,
                     })
+
+                    if num_files > 1 then
+                        lk.qf_populate(entries, "r")
+                    end
                 end
-
-                local short_uri = string.sub(vim.uri_to_fname(uri), #vim.fn.getcwd() + 2)
-                notification = notification .. string.format("made %d change(s) in %s", #edits, short_uri)
-            end
-
-            vim.notify(notification, 2, {
-                title = string.format("[LSP] rename: %s -> %s", position_params.oldName, position_params.newName),
-                timeout = 2500,
-            })
-
-            if num_files > 1 then
-                lk.qf_populate(entries, "r")
-            end
-        end)
-    end)
+            )
+        end
+    )
 end
 vim.lsp.buf.rename = qf_rename
 
