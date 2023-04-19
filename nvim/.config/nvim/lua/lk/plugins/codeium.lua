2be3d819de1d7dd857ad8ae9c606efc183551642
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-04-19 18:33:46 +0530

    feat(codeium): disable in `TelescopePrompt`

--- nvim/.config/nvim/lua/lk/plugins/codeium.lua
+++ nvim/.config/nvim/lua/lk/plugins/codeium.lua
@@ -7,6 +7,10 @@ M.config = function()
     vim.keymap.set("i", "<Tab>", function()
         return vim.fn["codeium#Accept"]()
     end, { expr = true })
+
+    vim.g.codeium_filetypes = {
+        TelescopePrompt = false,
+    }
 end
 
 return M
