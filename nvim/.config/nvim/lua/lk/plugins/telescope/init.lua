b9932e52aa4a0401a614140bfc9391fca57657c2
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-04-25 15:03:32 +0530

    fix(telescope-tabs): remove `entry_formatter`

--- nvim/.config/nvim/lua/lk/plugins/telescope/init.lua
+++ nvim/.config/nvim/lua/lk/plugins/telescope/init.lua
@@ -78,28 +78,7 @@ local M = {
         {
             "LukasPietzschmann/telescope-tabs",
             init = function()
-                require("telescope-tabs").setup({
-                    entry_formatter = function(
-                        tab_id,
-                        buffer_ids,
-                        file_names,
-                        file_paths,
-                        is_current
-                    )
-                        local entry_string = table.concat(
-                            vim.tbl_map(function(v)
-                                return vim.fn.fnamemodify(v, ":.")
-                            end, file_paths),
-                            ", "
-                        )
-                        return string.format(
-                            "%d: %s%s",
-                            tab_id,
-                            entry_string,
-                            is_current and " <" or ""
-                        )
-                    end,
-                })
+                require("telescope-tabs").setup()
             end,
         },
     },
