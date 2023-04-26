154b69b101a737b92a1501c93a36227b3afdcb7e
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-04-26 22:00:36 +0530

    feat(fzf-ueberzogen): move `ueberzogen` -> `bin`

--- fzf/fzf-ueberzogen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-[ -n "$PYENV_DEBUG" ] && set -x
-
-program="${0##*/}"
-
-export PYENV_ROOT="/home/lalitmee/.pyenv"
-exec "/home/lalitmee/.pyenv/libexec/pyenv" exec "$program" "$@"
