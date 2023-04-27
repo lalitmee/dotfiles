737f27fb13523bd3ab94ae08db41382c0c98ddea
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-04-27 17:31:38 +0530

    fix(auto-session): fix `session-lens`
    `session-lens` is merged into `auto-session`, so had to change the
    keybindings and the `dependencies` in the `installation`

--- nvim/.config/nvim/lua/lk/plugins/sessions.lua
+++ nvim/.config/nvim/lua/lk/plugins/sessions.lua
@@ -1,31 +1,13 @@
 local M = {
     "rmagatti/auto-session",
     event = { "VimEnter" },
-    dependencies = {
-        "rmagatti/session-lens",
-        cmd = { "SearchSession" },
-    },
 }
 
- M.config = function()
-    local actions = require("telescope.actions")
-
-    -- session-lens
-    require("telescope").load_extension("session-lens")
-
+M.config = function()
     require("auto-session").setup({
         auto_save_enabled = true,
         auto_restore_enabled = true,
         auto_session_use_git_branch = true,
-    })
-
-    require("session-lens").setup({
-        path_display = { "shorten" },
-        theme_conf = {
-            mappings = {
-                i = { ["<esc>"] = actions.close },
-            },
-        },
     })
 end
 
