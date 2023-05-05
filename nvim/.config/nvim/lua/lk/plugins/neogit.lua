c3205c7bec7d64086bd3ccba4d8b55a9a0a9e67c
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-05 15:27:43 +0530

    refactor(autocmds): remove redundant autocmds

--- nvim/.config/nvim/lua/lk/plugins/neogit.lua
+++ nvim/.config/nvim/lua/lk/plugins/neogit.lua
@@ -1,66 +1,59 @@
-local M = {
+local command = lk.command
+local augroup = lk.augroup
+
+return {
     "TimUntersberger/neogit",
     cmd = { "Neogit" },
     enabled = false,
+    config = function()
+        local neogit = require("neogit")
+
+        -- setup
+        neogit.setup({
+            disable_commit_confirmation = true,
+            integrations = { diffview = true },
+        })
+
+        -- commands
+        command("Neogit", function()
+            neogit.open()
+        end, {})
+
+        -- autocmds
+        local neogit_notify = function(msg, type)
+            local msg_type = type or "info"
+            vim.notify(msg, msg_type, { title = " Neogit" })
+        end
+
+        augroup("neogit_au", {
+            {
+                event = { "User" },
+                pattern = { "NeogitStatusRefreshed" },
+                command = function()
+                    neogit_notify("status has been reloaded")
+                end,
+            },
+            {
+                event = { "User" },
+                pattern = { "NeogitCommitComplete" },
+                command = function()
+                    neogit_notify("commited successfully")
+                end,
+            },
+            {
+                event = { "User" },
+                pattern = { "NeogitPushComplete" },
+                command = function()
+                    neogit_notify("pushed successfully")
+                end,
+            },
+            {
+                event = { "User" },
+                pattern = { "NeogitPushComplete" },
+                command = function()
+                    require("neogit").close()
+                end,
+            },
+        })
+    end,
 }
-
-M.config = function()
-    local neogit = require("neogit")
-
-    ----------------------------------------------------------------------
-    -- NOTE: setup {{{
-    ----------------------------------------------------------------------
-    neogit.setup({
-        disable_commit_confirmation = true,
-        integrations = { diffview = true },
-    })
-    -- }}}
-    ----------------------------------------------------------------------
-
-    --------------------------------------------------------------------------------
-    --  NOTE: command {{{
-    --------------------------------------------------------------------------------
-    local command = lk.command
-
-    command("Neogit", function()
-        neogit.open()
-    end, {})
-    -- }}}
-    --------------------------------------------------------------------------------
-
-    ----------------------------------------------------------------------
-    -- NOTE: autocmds {{{
-    ----------------------------------------------------------------------
-    --[[ local neogit_notify = function(msg, type) ]]
-    --[[   local msg_type = type or "info" ]]
-    --[[   vim.notify(msg, msg_type, { title = " Neogit" }) ]]
-    --[[ end ]]
-    --[[]]
-    --[[ lk.augroup("neogit_au", { ]]
-    --[[   { ]]
-    --[[     event = { "User" }, ]]
-    --[[     pattern = { "NeogitStatusRefreshed" }, ]]
-    --[[     command = function() ]]
-    --[[       neogit_notify("status has been reloaded") ]]
-    --[[     end, ]]
-    --[[   }, ]]
-    --[[   { ]]
-    --[[     event = { "User" }, ]]
-    --[[     pattern = { "NeogitCommitComplete" }, ]]
-    --[[     command = function() ]]
-    --[[       neogit_notify("commited successfully") ]]
-    --[[     end, ]]
-    --[[   }, ]]
-    --[[   { ]]
-    --[[     event = { "User" }, ]]
-    --[[     pattern = { "NeogitPushComplete" }, ]]
-    --[[     command = function() ]]
-    --[[       neogit_notify("pushed successfully") ]]
-    --[[     end, ]]
-    --[[   }, ]]
-    --[[ }) ]]
-    -- }}}
-    ----------------------------------------------------------------------
-end
-
-return M
