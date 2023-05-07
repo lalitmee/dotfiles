650255aa8e0f448884ffe5a4c8b2291c6edb36f1
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-07 16:51:55 +0530

    feat(auto-session): save `tabnames` from tabby

--- nvim/.config/nvim/lua/lk/plugins/sessions.lua
+++ nvim/.config/nvim/lua/lk/plugins/sessions.lua
@@ -8,6 +8,31 @@ M.config = function()
         auto_save_enabled = true,
         auto_restore_enabled = true,
         auto_session_use_git_branch = true,
+        save_extra_cmds = {
+            -- tabby: tabs name
+            function()
+                local cmds = {}
+                for _, t in pairs(vim.api.nvim_list_tabpages()) do
+                    local tabname = require("tabby.feature.tab_name").get_raw(t)
+                    if tabname ~= "" then
+                        table.insert(
+                            cmds,
+                            'pcall(require("tabby.feature.tab_name").set, '
+                                .. t
+                                .. ', "'
+                                .. tabname:gsub('"', '\\"')
+                                .. '")'
+                        )
+                    end
+                end
+
+                if #cmds == 0 then
+                    return ""
+                end
+
+                return "lua " .. table.concat(cmds, ";")
+            end,
+        },
     })
 end
 
