bf88db1ae6bcec1620c57a691ea0f8b16fede127
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-10 11:05:40 +0530

    refactor(vim-tmux-navigator): moved kebindings

---
+++ nvim/.config/nvim/lua/lk/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,22 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+        "<C-h>",
+        "<C-j>",
+        "<C-k>",
+        "<C-l>",
+        "<C-/>",
+    },
+    init = function()
+        local wk = require("which-key")
+        wk.register({
+            ["w"] = {
+                ["h"] = { ":<C-U>TmuxNavigateLeft<CR>", "window-left" },
+                ["j"] = { ":<C-U>TmuxNavigateDown<CR>", "window-down" },
+                ["k"] = { ":<C-U>TmuxNavigateUp<CR>", "window-up" },
+                ["l"] = { ":<C-U>TmuxNavigateRight<CR>", "window-right" },
+                ["p"] = { ":<C-U>TmuxNavigatePrevious<CR>", "window-previous" },
+            },
+        }, { mode = "n", prefix = "<leader>" })
+    end,
+}
