ea2891948600f304882be501d98cb47970928247
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-10 10:24:47 +0530

    feat(zellij): trying to create layout for `work`

---
+++ nvim/.config/nvim/after/ftplugin/kdl.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt_local
+
+opt.formatoptions = opt.formatoptions - "o" -- don't continue comments on `o` and `O`
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.textwidth = 80
+opt.foldlevel = 99
