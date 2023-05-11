41ef50be1847cbc089421b102e32b47df2e67601
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-11 22:56:03 +0530

    refactor(git): move `gh.nvim` and `octo.nvim`

--- nvim/.config/nvim/lua/lk/plugins/git.lua
+++ nvim/.config/nvim/lua/lk/plugins/git.lua
@@ -275,10 +275,214 @@ local function gitsigns()
     }
 end
 
+local function octo()
+    return {
+        "pwntester/octo.nvim",
+        cmd = { "Octo" },
+        config = true,
+        init = function()
+            local wk = require("which-key")
+            wk.register({
+                ["g"] = {
+                    ["o"] = {
+                        ["name"] = "+octo.nvim",
+                        ["a"] = {
+                            ["name"] = "+reaction",
+                            ["c"] = {
+                                ":Octo reaction confused<CR>",
+                                "react-confused",
+                            },
+                            ["d"] = {
+                                ":Octo reaction thumbs_down<CR>",
+                                "react-thumbs_down",
+                            },
+                            ["e"] = { ":Octo reaction eyes<CR>", "react-eyes" },
+                            ["h"] = {
+                                ":Octo reaction heart<CR>",
+                                "react-heart",
+                            },
+                            ["l"] = {
+                                ":Octo reaction laugh<CR>",
+                                "react-laugh",
+                            },
+                            ["r"] = {
+                                ":Octo reaction rocket<CR>",
+                                "react-rocket",
+                            },
+                            ["t"] = { ":Octo reaction tada<CR>", "react-tada" },
+                            ["u"] = {
+                                ":Octo reaction thumbs_up<CR>",
+                                "react-thumbs_up",
+                            },
+                        },
+                        ["c"] = {
+                            ["name"] = "+comment",
+                            ["a"] = { ":Octo comment add<CR>", "add" },
+                            ["d"] = { ":Octo comment delete<CR>", "delete" },
+                        },
+                        ["g"] = { ":Octo gist list<CR>", "list-gist" },
+                        ["i"] = {
+                            ["name"] = "+issues",
+                            ["a"] = { ":Octo issue create<CR>", "create" },
+                            ["b"] = { ":Octo issue browser<CR>", "browser" },
+                            ["c"] = { ":Octo issue close<CR>", "close" },
+                            ["e"] = { ":Octo issue edit<CR>", "edit" },
+                            ["l"] = { ":Octo issue list<CR>", "list" },
+                            ["o"] = { ":Octo issue reopen<CR>", "reopen" },
+                            ["r"] = { ":Octo issue reload<CR>", "reload" },
+                            ["s"] = { ":Octo issue search<CR>", "search" },
+                            ["u"] = { ":Octo issue url<CR>", "url" },
+                        },
+                        ["l"] = {
+                            ["name"] = "+label",
+                            ["a"] = { ":Octo label add<CR>", "add" },
+                            ["c"] = { ":Octo label create<CR>", "create" },
+                            ["r"] = { ":Octo label remove<CR>", "remove" },
+                        },
+                        ["p"] = {
+                            ["name"] = "+pull-requests",
+                            ["a"] = { ":Octo pr create<CR>", "create" },
+                            ["b"] = { ":Octo pr browser<CR>", "browser" },
+                            ["c"] = { ":Octo pr checkout<CR>", "checkout" },
+                            ["C"] = { ":Octo pr close<CR>", "close" },
+                            ["d"] = { ":Octo pr diff<CR>", "diff" },
+                            ["D"] = { ":Octo pr checks<CR>", "checks" },
+                            ["e"] = { ":Octo pr edit<CR>", "edit" },
+                            ["g"] = { ":Octo pr commits<CR>", "commits" },
+                            ["h"] = { ":Octo pr changes<CR>", "changes" },
+                            ["l"] = { ":Octo pr list<CR>", "list" },
+                            ["m"] = { ":Octo pr merge<CR>", "merge" },
+                            ["o"] = { ":Octo pr reopen<CR>", "reopen" },
+                            ["r"] = { ":Octo pr reload<CR>", "reload" },
+                            ["R"] = { ":Octo pr ready<CR>", "ready" },
+                            ["s"] = { ":Octo pr search<CR>", "search" },
+                            ["u"] = { ":Octo pr url<CR>", "url" },
+                        },
+                        ["r"] = {
+                            ["name"] = "+repositories",
+                            ["b"] = { ":Octo repo browser<CR>", "browser" },
+                            ["f"] = { ":Octo repo fork<CR>", "fork" },
+                            ["l"] = { ":Octo repo list<CR>", "list" },
+                            ["u"] = { ":Octo repo url<CR>", "url" },
+                        },
+                        ["R"] = {
+                            ["name"] = "+review",
+                            ["a"] = { ":Octo reviewer add<CR>", "add-reviewer" },
+                            ["b"] = { ":Octo review start<CR>", "start-review" },
+                            ["c"] = {
+                                ":Octo review comments<CR>",
+                                "comments-review",
+                            },
+                            ["d"] = {
+                                ":Octo review discard<CR>",
+                                "discard-review",
+                            },
+                            ["r"] = {
+                                ":Octo review resume<CR>",
+                                "resume-review",
+                            },
+                            ["s"] = {
+                                ":Octo review submit<CR>",
+                                "submit-review",
+                            },
+                        },
+                        ["t"] = {
+                            ["name"] = "+thread",
+                            ["r"] = { ":Octo thread resolve<CR>", "resolve" },
+                            ["u"] = {
+                                ":Octo thread unresolve<CR>",
+                                "unresolve",
+                            },
+                        },
+                    },
+                },
+            }, { mode = "n", prefix = "<leader>" })
+        end,
+    }
+end
+
+local function gh_nvim()
+    return {
+        "ldelossa/gh.nvim",
+        dependencies = { "ldelossa/litee.nvim" },
+        cmd = {
+            "GHOpenIssue",
+            "GHOpenPR",
+            "GHSearchIssues",
+            "GHSearchPRs",
+        },
+        config = function()
+            require("litee.lib").setup()
+            require("litee.gh").setup()
+        end,
+        init = function()
+            local wk = require("which-key")
+            wk.register({
+                ["g"] = {
+                    ["H"] = {
+                        ["name"] = "+gh.nvim",
+                        ["f"] = {
+                            ":Telescope gh pull_request_files<CR>",
+                            "gh-pr-files",
+                        },
+                        ["g"] = { ":Telescope gh gist<CR>", "gh-gist" },
+                        ["I"] = { ":Telescope gh issues<CR>", "gh-issues" },
+                        ["P"] = { ":Telescope gh pull_request<CR>", "gh-pr" },
+                        ["c"] = {
+                            ["name"] = "+Commits",
+                            ["c"] = { "<cmd>GHCloseCommit<cr>", "Close" },
+                            ["e"] = { "<cmd>GHExpandCommit<cr>", "Expand" },
+                            ["o"] = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+                            ["p"] = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+                            ["z"] = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+                        },
+                        ["i"] = {
+                            ["name"] = "+Issues",
+                            ["p"] = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+                        },
+                        ["l"] = {
+                            ["name"] = "+Litee",
+                            ["t"] = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+                        },
+                        ["r"] = {
+                            ["name"] = "+Review",
+                            ["b"] = { "<cmd>GHStartReview<cr>", "Begin" },
+                            ["c"] = { "<cmd>GHCloseReview<cr>", "Close" },
+                            ["d"] = { "<cmd>GHDeleteReview<cr>", "Delete" },
+                            ["e"] = { "<cmd>GHExpandReview<cr>", "Expand" },
+                            ["s"] = { "<cmd>GHSubmitReview<cr>", "Submit" },
+                            ["z"] = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+                        },
+                        ["p"] = {
+                            ["name"] = "+Pull Request",
+                            ["c"] = { "<cmd>GHClosePR<cr>", "Close" },
+                            ["d"] = { "<cmd>GHPRDetails<cr>", "Details" },
+                            ["e"] = { "<cmd>GHExpandPR<cr>", "Expand" },
+                            ["o"] = { "<cmd>GHOpenPR<cr>", "Open" },
+                            ["p"] = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+                            ["r"] = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+                            ["t"] = { "<cmd>GHOpenToPR<cr>", "Open To" },
+                            ["z"] = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+                        },
+                        ["t"] = {
+                            ["name"] = "+Threads",
+                            ["c"] = { "<cmd>GHCreateThread<cr>", "Create" },
+                            ["n"] = { "<cmd>GHNextThread<cr>", "Next" },
+                            ["t"] = { "<cmd>GHToggleThread<cr>", "Toggle" },
+                        },
+                    },
+                },
+            }, { mode = "n", prefix = "<leader>" })
+        end,
+    }
+end
+
 return {
-    neogit(),
-    fugitive(),
     advanced_git_search(),
-    igs(),
+    fugitive(),
+    gh_nvim(),
     gitsigns(),
+    igs(),
+    neogit(),
+    octo(),
 }
