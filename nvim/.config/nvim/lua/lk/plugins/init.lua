2f39679d143592bd2405dc67d9f1b548074e10c6
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-12 09:54:34 +0530

    refactor(languages): move all to `languages.lua`

--- nvim/.config/nvim/lua/lk/plugins/init.lua
+++ nvim/.config/nvim/lua/lk/plugins/init.lua
@@ -86,49 +86,13 @@ return {
     ---------------------------------------------------------------------------
 
     ---------------------------------------------------------------------------
-    --  NOTE: languages {{{
+    --  NOTE: enhancements {{{
     ---------------------------------------------------------------------------
     {
         "NMAC427/guess-indent.nvim",
         event = { "VeryLazy" },
         opts = {},
     },
-    {
-        "Vimjas/vim-python-pep8-indent",
-        ft = "python",
-    },
-    {
-        "baskerville/vim-sxhkdrc",
-        ft = { "sxhkdrc" },
-    },
-    {
-        "antonk52/markdowny.nvim",
-        ft = { "markdown" },
-        keys = {
-            { "<C-b>", mode = "v" },
-            { "<C-k>", mode = "v" },
-            { "<C-i>", mode = "v" },
-        },
-        opts = {},
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-        ft = "markdown",
-        cmd = { "MarkdownPreview" },
-    },
-    {
-        "ray-x/go.nvim",
-        ft = "go",
-    },
-    -- }}}
-    ---------------------------------------------------------------------------
-
-    ---------------------------------------------------------------------------
-    --  NOTE: enhancements {{{
-    ---------------------------------------------------------------------------
     {
         "nvim-colortils/colortils.nvim",
         cmd = { "Colortils" },
