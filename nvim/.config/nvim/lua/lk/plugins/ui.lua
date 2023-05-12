43cd09eebb190d04c21045a459f8e0218fac9ac0
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-05-12 10:25:36 +0530

    refactor(ui): move `plugins` to `ui.lua`
    - noice
    - notify
    - devicons

--- nvim/.config/nvim/lua/lk/plugins/ui.lua
+++ nvim/.config/nvim/lua/lk/plugins/ui.lua
@@ -1,6 +1,7 @@
+local api = vim.api
 local border, L = lk.style.border.rounded, vim.log.levels
 
-local M = {
+local noice = {
     "folke/noice.nvim",
     event = { "VeryLazy" },
     opts = {
@@ -193,8 +194,100 @@ local M = {
             bottom_search = false,
         },
     },
+    enabled = false,
 }
 
-M.enabled = false
+local nvim_notify = {
+    "rcarriga/nvim-notify",
+    event = { "VeryLazy" },
+    init = function()
+        local notify = require("notify")
 
-return M
+        notify.setup({
+            -- background_colour = "NormalFloat",
+            timeout = 3000,
+            stages = "slide",
+            max_width = function()
+                return math.floor(vim.go.columns * 0.8)
+            end,
+            max_height = function()
+                return math.floor(vim.go.lines * 0.8)
+            end,
+            on_open = function(win)
+                if api.nvim_win_is_valid(win) then
+                    vim.api.nvim_win_set_config(win, {
+                        border = {
+                            "╭",
+                            "─",
+                            "╮",
+                            "│",
+                            "╯",
+                            "─",
+                            "╰",
+                            "│",
+                        },
+                    })
+                end
+            end,
+            render = function(...)
+                local notif = select(2, ...)
+                local style = notif.title[1] == "" and "minimal" or "default"
+                require("notify.render")[style](...)
+            end,
+            icons = lk.style.icons.notify,
+        })
+        vim.notify = notify
+
+        local notify_filter = vim.notify
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.notify = function(msg, ...)
+            if
+                msg:match("warning: multiple different client offset_encodings")
+            then
+                return
+            end
+            if msg:match("character_offset must be called") then
+                return
+            end
+            if msg:match("No information available") then
+                return
+            end
+
+            notify_filter(msg, ...)
+        end
+
+        lk.command("NotifyDismiss", notify.dismiss, {})
+    end,
+}
+
+local smartcolumn = {
+    "m4xshen/smartcolumn.nvim",
+    event = { "BufReadPost" },
+    config = function()
+        require("smartcolumn").setup({
+            disabled_filetypes = {
+                "toggleterm",
+                "lazy",
+                "mason",
+                "help",
+            },
+        })
+    end,
+}
+
+local devicons = {
+    "yamatsum/nvim-nonicons",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+        require("nvim-web-devicons").setup({
+            get_icons = require("nvim-nonicons"),
+        })
+    end,
+}
+
+return {
+    devicons,
+    noice,
+    nvim_notify,
+    smartcolumn,
+}
