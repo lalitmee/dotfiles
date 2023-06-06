60169b3dd7e638d981afadb34859fb323c4c9a27
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-06-07 00:44:48 +0530

    feat(comment-frame): add comment frame

---
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "s1n7ax/nvim-comment-frame",
+        opts = {
+            keymap = "<localleader>cc",
+            multiline_keymap = "<localleader>cC",
+        },
+        keys = {
+            "<localleader>cc",
+            "<localleader>cC",
+        },
+    },
+}
