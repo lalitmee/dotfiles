b73756b5a9d938d748a95bb377bbd533366d85bd
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-06-07 20:25:47 +0530

    refactor(oil): change lua module structure

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,40 +1,26 @@
-local M = {
+return {
     "stevearc/oil.nvim",
     keys = { "-" },
     cmd = { "Oil" },
-}
-
-M.init = function()
-    lk.command("OilFloat", require("oil").open_float, {})
-    vim.keymap.set(
-        "n",
-        "-",
-        require("oil").open_float,
-        { desc = "Open parent directory" }
-    )
-end
-
-M.config = function()
-    require("oil").setup({
-        columns = {
-            "icon",
-            -- "permissions",
-            -- "size",
-            -- "mtime",
-        },
-        float = {
-            win_options = {
-                winblend = 0,
+    init = function()
+        lk.command("OilFloat", require("oil").open_float, {})
+        vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory" })
+    end,
+    config = function()
+        require("oil").setup({
+            columns = { "icon" },
+            float = {
+                win_options = {
+                    winblend = 0,
+                },
+                padding = 5,
+                max_height = 120,
+                max_width = 160,
             },
-            padding = 5,
-            max_height = 120,
-            max_width = 160,
-        },
-        view_options = {
-            show_hidden = true,
-        },
-        skip_confirm_for_simple_edits = true,
-    })
-end
-
-return M
+            view_options = {
+                show_hidden = true,
+            },
+            skip_confirm_for_simple_edits = true,
+        })
+    end,
+}
