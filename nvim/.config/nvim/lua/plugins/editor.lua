d256ee0489a11ea46a1babd3fc86d70b89763110
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-07-14 00:15:54 +0530

    feat(flash): add `flash.nvim` new keybindings

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -562,6 +562,22 @@ return {
                 end,
                 desc = "Remote Flash",
             },
+            {
+                "R",
+                mode = { "o", "x" },
+                function()
+                    require("flash").treesitter_search()
+                end,
+                desc = "Flash Treesitter Search",
+            },
+            {
+                "<c-s>",
+                mode = { "c" },
+                function()
+                    require("flash").toggle()
+                end,
+                desc = "Toggle Flash Search",
+            },
         },
         init = function()
             local wk = require("which-key")
