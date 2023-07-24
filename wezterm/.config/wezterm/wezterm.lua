5b19383f76377f28a9e6d7d1dda8276190511a5c
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-07-25 00:10:02 +0530

    feat(wezterm): add config for wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.enable_tab_bar = false
+
+-- For example, changing the color scheme:
+config.color_scheme = "Cobalt2"
+
+config.font = wezterm.font("Operator Mono Lig Book")
+config.font_size = 11
+
+-- and finally, return the configuration to wezterm
+return config
