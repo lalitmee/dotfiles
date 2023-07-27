3667ee07409919d1accf66bacf5f428eb22c240b
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-07-27 09:39:25 +0530

    fix(wezterm): add something

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,6 +13,15 @@ end
 -- This is where you actually apply your config choices
 config.enable_tab_bar = false
 
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+config.window_background_opacity = 0.8
+
 -- For example, changing the color scheme:
 config.color_scheme = "Cobalt2"
 
