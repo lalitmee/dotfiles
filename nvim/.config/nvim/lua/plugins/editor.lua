eff0b4ef0ee6e5fd187f79d2cf7a9302767f4861
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-08-11 10:38:32 +0530

    refactor(vim-matchup): remove `init` function

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -521,24 +521,7 @@ return {
 
     { --[[ matchup ]]
         "andymass/vim-matchup",
-        event = { "VeryLazy" },
-        init = function()
-            lk.augroup("vim_matchup_au", {
-                {
-                    event = { "FileType" },
-                    pattern = { "javascript" },
-                    command = function()
-                        vim.cmd([[
-                            function! JsxHotfix()
-                                setlocal matchpairs=(:),{:},[:],<:>
-                                let b:match_words = '<\@<=\([^/][^ \t>]*\)\g{hlend}[^>]*\%(/\@<!>\|$\):<\@<=/\1>'
-                            endfunction
-                            let g:matchup_hotfix = { 'javascript': 'JsxHotfix' }
-                        ]])
-                    end,
-                },
-            })
-        end,
+        event = "VeryLazy",
         config = function()
             vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
         end,
