13bfc97016f32e37f28ec03be0b7e18b11775d14
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-08-30 01:23:28 +0530

    feat(neovide): add `neovide` `after/plugin`

---
+++ nvim/.config/nvim/after/plugin/neovide.lua
@@ -0,0 +1,14 @@
+if vim.g.neovide then
+    -- font
+    vim.o.guifont = "Operator Mono Lig Book:h11"
+
+    -- floating blur
+    vim.g.neovide_floating_blur_amount_x = 2.0
+    vim.g.neovide_floating_blur_amount_y = 2.0
+
+    -- transparency
+    vim.g.neovide_transparency = 0.8
+
+    -- theme
+    vim.g.neovide_theme = "auto"
+end
