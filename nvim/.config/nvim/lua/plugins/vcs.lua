46645b8474d6ca24d484cc8bd88f74d254bbcbfc
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-09-26 14:15:55 +0530

    refactor(nvim): remove custom `neogit-notify`

--- nvim/.config/nvim/lua/plugins/vcs.lua
+++ nvim/.config/nvim/lua/plugins/vcs.lua
@@ -9,10 +9,10 @@ return {
         },
         init = function()
             -- autocmds
-            local neogit_notify = function(msg, type)
-                local msg_type = type or "info"
-                vim.notify(msg, msg_type, { title = " Neogit" })
-            end
+            -- local neogit_notify = function(msg, type)
+            --     local msg_type = type or "info"
+            --     vim.notify(msg, msg_type, { title = " Neogit" })
+            -- end
 
             augroup("neogit_au", {
                 {
@@ -22,20 +22,20 @@ return {
                         vim.cmd([[set ft=gitcommit]])
                     end,
                 },
-                {
-                    event = { "User" },
-                    pattern = { "NeogitCommitComplete" },
-                    command = function()
-                        neogit_notify("Commited Successfully")
-                    end,
-                },
-                {
-                    event = { "User" },
-                    pattern = { "NeogitPushComplete" },
-                    command = function()
-                        neogit_notify("Pushed Successfully")
-                    end,
-                },
+                -- {
+                --     event = { "User" },
+                --     pattern = { "NeogitCommitComplete" },
+                --     command = function()
+                --         neogit_notify("Commited Successfully")
+                --     end,
+                -- },
+                -- {
+                --     event = { "User" },
+                --     pattern = { "NeogitPushComplete" },
+                --     command = function()
+                --         neogit_notify("Pushed Successfully")
+                --     end,
+                -- },
                 {
                     event = { "User" },
                     pattern = { "NeogitPushComplete" },
