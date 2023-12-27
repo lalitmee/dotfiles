c551544717e05498bbc4892b9a59056c6dac37d8
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-12-28 01:32:11 +0530

    fix(kitty): update kitty font

--- nvim/.config/nvim/plugin/kitty.lua
+++ nvim/.config/nvim/plugin/kitty.lua
@@ -4,7 +4,6 @@ lk.augroup("kitty_au", {
         pattern = { "kitty.conf" },
         command = function()
             vim.cmd([[:silent !kill -SIGUSR1 $(pgrep kitty)]])
-            vim.notify("config reloaded", 2, { title = "Kitty Config" })
         end,
     },
 })
