3998a0348c3eef95c0783a21018a43d122b2577e
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-01-01 01:02:05 +0530

    fix(neovide): update `config` file name
    - `setting.toml` -> `settings.toml`
    - updated font size from 11 to 12

--- nvim/.config/nvim/after/plugin/neovide.lua
+++ nvim/.config/nvim/after/plugin/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
     -- font
-    vim.o.guifont = "Operator Mono Lig Book:h11"
+    vim.o.guifont = "Operator Mono Lig Book:h12"
 
     -- floating blur
     vim.g.neovide_floating_blur_amount_x = 2.0
