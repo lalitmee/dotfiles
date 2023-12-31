2897ef02369b28cf6a28c57d50d50cc26612be35
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-12-31 23:31:31 +0530

    refactor(nvim): remove `cmp-git` from `nvim-cmp`
    I am not using `cmp-git` anyways and I don't think I will need this in
    future.

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -10,8 +10,7 @@ return {
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-path",
             "onsails/lspkind.nvim",
-            "petertriho/cmp-git",
-            { "saadparwaiz1/cmp_luasnip", dependencies = { "L3MON4D3/LuaSnip" } },
+            "saadparwaiz1/cmp_luasnip",
         },
         config = function()
             local cmp = require("cmp")
