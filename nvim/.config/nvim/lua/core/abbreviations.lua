d78b5489e5c8cad008b894bd2d004efb9e3b0f89
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-02-13 01:36:15 +0530

    fix(nvim): disable `abbreviations.lua`
    it breaks the newly installed configuration and anyways its not working
    as expected. Need to figure it out later

--- nvim/.config/nvim/lua/core/abbreviations.lua
+++ nvim/.config/nvim/lua/core/abbreviations.lua
@@ -1,19 +1,19 @@
-local function iabbrev(lhs, rhs)
-    vim.keymap.set("ia", lhs, rhs)
-end
+-- local function iabbrev(lhs, rhs)
+--     vim.keymap.set("ia", lhs, rhs)
+-- end
 
-local function cabbrev(lhs, rhs)
-    vim.keymap.set("ca", lhs, rhs)
-end
+-- local function cabbrev(lhs, rhs)
+--     vim.keymap.set("ca", lhs, rhs)
+-- end
 
-----------------------------------------------------------------------
---                    command line abbreviations                    --
-----------------------------------------------------------------------
-cabbrev("wq", [[execute "Format sync" <bar> wq]])
+-- ----------------------------------------------------------------------
+-- --                    command line abbreviations                    --
+-- ----------------------------------------------------------------------
+-- cabbrev("wq", [[execute "Format sync" <bar> wq]])
 
-----------------------------------------------------------------------
---                       normal abbreviations                       --
-----------------------------------------------------------------------
-iabbrev("teh", "the")
-iabbrev("tihs", "this")
-iabbrev("funciton", "function")
+-- ----------------------------------------------------------------------
+-- --                       normal abbreviations                       --
+-- ----------------------------------------------------------------------
+-- iabbrev("teh", "the")
+-- iabbrev("tihs", "this")
+-- iabbrev("funciton", "function")
