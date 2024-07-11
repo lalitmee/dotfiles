c187ae449847020260dba85d547ecb6044ae4d49
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-07-11 21:08:08 +0530

    fix(codeium): use default keybindings

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -45,20 +45,6 @@ return {
         "Exafunction/codeium.vim",
         event = "BufEnter",
         config = function()
-            -- Change '<C-g>' here to any keycode you like.
-            vim.keymap.set("i", "<Tab>", function()
-                return vim.fn["codeium#Accept"]()
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<c-;>", function()
-                return vim.fn["codeium#CycleCompletions"](1)
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<c-,>", function()
-                return vim.fn["codeium#CycleCompletions"](-1)
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<c-x>", function()
-                return vim.fn["codeium#Clear"]()
-            end, { expr = true, silent = true })
-
             vim.g.codeium_filetypes_disabled_by_default = true
 
             vim.g.codeium_filetypes = {
