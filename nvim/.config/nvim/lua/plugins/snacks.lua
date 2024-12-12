4947c05400d8f9b780ed072669a0969bd2f5f9fa
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-12-12 11:53:28 +0530

    feat(snacks): add more things from config

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -27,6 +27,26 @@ return {
 
         { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
         { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+
+        -- neovim news
+        {
+            "<leader>N",
+            desc = "Neovim News",
+            function()
+                Snacks.win({
+                    file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+                    width = 0.6,
+                    height = 0.6,
+                    wo = {
+                        spell = false,
+                        wrap = false,
+                        signcolumn = "yes",
+                        statuscolumn = " ",
+                        conceallevel = 3,
+                    },
+                })
+            end,
+        }
     },
     opts = {
         bigfile = { enabled = true },
@@ -59,5 +79,39 @@ return {
                 wo = { wrap = true }, -- Wrap notifications
             },
         },
+        scroll = {},
     },
+    -- NOTE: there is some error with `Snacks.toggle`, will try this later on
+    -- init = function()
+    --     vim.api.nvim_create_autocmd("User", {
+    --         pattern = "VeryLazy",
+    --         callback = function()
+    --             -- Setup some globals for debugging (lazy-loaded)
+    --             _G.dd = function(...)
+    --                 Snacks.debug.inspect(...)
+    --             end
+    --             _G.bt = function()
+    --                 Snacks.debug.backtrace()
+    --             end
+    --             vim.print = _G.dd -- Override print to use snacks for `:=` command
+    --
+    --             -- Create some toggle mappings
+    --             Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+    --             Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+    --             Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+    --             Snacks.toggle.diagnostics():map("<leader>ud")
+    --             Snacks.toggle.line_number():map("<leader>ul")
+    --             Snacks.toggle
+    --                 .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+    --                 :map("<leader>uc")
+    --             Snacks.toggle.treesitter():map("<leader>uT")
+    --             Snacks.toggle
+    --                 .option("background", { off = "light", on = "dark", name = "Dark Background" })
+    --                 :map("<leader>ub")
+    --             Snacks.toggle.inlay_hints():map("<leader>uh")
+    --             Snacks.toggle.indent():map("<leader>ug")
+    --             Snacks.toggle.dim():map("<leader>uD")
+    --         end,
+    --     })
+    -- end,
 }
