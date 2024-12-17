44cd969f17f10774ea01afeb8d33cf0d32839a80
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-12-17 11:32:52 +0530

    fix(json): change shiftwidth from 4 to 2

--- nvim/.config/nvim/after/ftplugin/json.lua
+++ nvim/.config/nvim/after/ftplugin/json.lua
@@ -1,9 +1,9 @@
 local opt = vim.opt_local
 
 opt.formatoptions = opt.formatoptions - "o" -- don't continue comments on `o` and `O`
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.tabstop = 4
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.tabstop = 2
 opt.expandtab = true
 
 -- When creating a new line with o, make sure there is a trailing comma on the
