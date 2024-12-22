7f88a1cb2b909f5c6a7a9cca989c7c57a45bf7ef
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-12-22 18:13:17 +0530

    feat(snippet): add git commit message snippet
    for todos

--- nvim/.config/nvim/luasnippets/gitcommit.lua
+++ nvim/.config/nvim/luasnippets/gitcommit.lua
@@ -7,6 +7,7 @@ local i = env["i"]
 local c = env["c"]
 local f = env["f"]
 local fmt = env["fmt"]
+local fmta = env["fmta"]
 
 return {
     s({
@@ -18,4 +19,9 @@ return {
         trig = "pk",
         name = "update package lock file",
     }, t("chore(lockfile): update packages")),
+
+    s({
+        trig = "gtodo",
+        name = "git commit for todo",
+    }, c(1, { t("feat(todo): add work todo"), t("feat(todo): add personal todo") })),
 }
