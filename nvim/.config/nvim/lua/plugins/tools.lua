dfc4a1666d946b8aed0af500519c0a2b16f2e7da
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-12-31 00:28:15 +0530

    fix(flatten): use the defaults for `flatten.nvim`

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -640,49 +640,9 @@ return {
 
     { --[[ flatten.nvim ]]
         "willothy/flatten.nvim",
-        opts = {
-            window = {
-                open = "alternate",
-            },
-            callbacks = {
-                should_block = function(argv)
-                    return vim.tbl_contains(argv, "-b")
-                end,
-                pre_open = function()
-                    local term = require("toggleterm.terminal")
-                    local termid = term.get_focused_id()
-                    Saved_terminal = term.get(termid)
-                end,
-                post_open = function(bufnr, winnr, ft, is_blocking)
-                    if is_blocking and Saved_terminal then
-                        Saved_terminal:close()
-                    else
-                        vim.api.nvim_set_current_win(winnr)
-                        require("wezterm").switch_pane.id(tonumber(os.getenv("WEZTERM_PANE")))
-                    end
-
-                    if ft == "gitcommit" or ft == "gitrebase" then
-                        vim.api.nvim_create_autocmd("BufWritePost", {
-                            buffer = bufnr,
-                            once = true,
-                            callback = vim.schedule_wrap(function()
-                                vim.api.nvim_buf_delete(bufnr, {})
-                            end),
-                        })
-                    end
-                end,
-                block_end = function()
-                    vim.schedule(function()
-                        if Saved_terminal then
-                            Saved_terminal:open()
-                            Saved_terminal = nil
-                        end
-                    end)
-                end,
-            },
-        },
         lazy = false,
         priority = 1001,
+        opts = {},
     },
 
     { --[[ http-codes ]]
