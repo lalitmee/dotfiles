32de995b6b66508db9da1f4b466bb2b4a638da80
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-04-04 02:29:40 +0530

    feat: improve font management and update configurations
    - Enhanced font installation script to preserve folder structure and
      avoid duplicate font copies.
    - Updated Ghostty configuration to use "Iosevka Nerd Font" instead of
      "Operator Mono Lig".
    - Modified Neovide settings to use "IosevkaTerm NF" font and adjusted
      opacity settings for better visual clarity.
    - Removed redundant Neovide settings from the main init.lua file.

--- nvim/.config/nvim/after/plugin/neovide.lua
+++ nvim/.config/nvim/after/plugin/neovide.lua
@@ -1,13 +1,14 @@
 if vim.g.neovide then
     -- font
-    vim.o.guifont = "Operator Mono Lig Book:h10"
+    -- vim.o.guifont = "Operator Mono Lig Book:h11"
+    vim.o.guifont = "IosevkaTerm NF:h11"
 
     -- floating blur
     vim.g.neovide_floating_blur_amount_x = 2.0
     vim.g.neovide_floating_blur_amount_y = 2.0
 
     -- transparency
-    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_opacity = 0.7
 
     -- theme
     vim.g.neovide_theme = "auto"
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,17 +32,4 @@ require("core")
 -- }}}
 ----------------------------------------------------------------------
 
---------------------------------------------------------------------------------
---  NOTE: neovide settings {{{
---------------------------------------------------------------------------------
-if vim.fn.exists("g:neovide") then
-    -- vim.g.neovide_fullscreen = true
-    -- vim.g.neovide_cursor_vfx_mode = "torpedo"
-    vim.g.neovide_transparency = 0.9
-    vim.g.neovide_floating_blur_amount_x = 2.0
-    vim.g.neovide_floating_blur_amount_y = 2.0
-end
--- }}}
---------------------------------------------------------------------------------
-
 -- vim:foldmethod=marker
