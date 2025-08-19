43be16204b2df00e56249a3bacd09afc77ed88da
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-08-19 22:08:30 +0530

    feat(neovide): update font and GUI config for BlexMono
    - switch primary font to "BlexMono Nerd Font Mono" and increase size in Neovide config
    - update neovide.lua to match new font and adjust opacity setting
    - comment out previous font options and deprecate old settings for consistency

--- nvim/.config/nvim/after/plugin/neovide.lua
+++ nvim/.config/nvim/after/plugin/neovide.lua
@@ -1,15 +1,16 @@
-if vim.g.neovide then
-    -- font
-    vim.o.guifont = "Operator Mono Lig Book:h11"
-    -- vim.o.guifont = "IosevkaTerm NF:h11"
-
-    -- floating blur
-    vim.g.neovide_floating_blur_amount_x = 2.0
-    vim.g.neovide_floating_blur_amount_y = 2.0
-
-    -- transparency
-    vim.g.neovide_opacity = 0.7
-
-    -- theme
-    vim.g.neovide_theme = "auto"
-end
+-- if vim.g.neovide then
+--     -- font
+--     vim.o.guifont = "BlexMono Nerd Font Mono:h12"
+--     -- vim.o.guifont = "Operator Mono Lig Book:h11"
+--     -- vim.o.guifont = "IosevkaTerm NF:h11"
+--
+--     -- floating blur
+--     vim.g.neovide_floating_blur_amount_x = 2.0
+--     vim.g.neovide_floating_blur_amount_y = 2.0
+--
+--     -- transparency
+--     vim.g.neovide_opacity = 0.9
+--
+--     -- theme
+--     vim.g.neovide_theme = "auto"
+-- end
