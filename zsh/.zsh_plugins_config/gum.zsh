64637aeb662e1fb73eb151d95b9117381659e855
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-09-16 22:59:10 +0530

    feat(zsh): add gum shell configuration with cobalt2 theme
    - create new gum.zsh configuration file with cobalt2 color scheme
    - configure styling for filter, choose, confirm, and input components
    - integrate gum configuration into zsh plugins initialization

---
+++ zsh/.zsh_plugins_config/gum.zsh
@@ -0,0 +1,27 @@
+# -------------------------------------------------------------------
+# NOTE: gum configuration (cobalt2 theme) {{{
+# -------------------------------------------------------------------
+# Global gum styling with cobalt2 colors
+# Based on tmux cobalt2 theme colors
+export GUM_FILTER_INDICATOR_FOREGROUND="#00AAFF"        # cobalt2 blue
+export GUM_FILTER_MATCH_FOREGROUND="#FFC600"           # cobalt2 yellow
+export GUM_FILTER_TEXT_FOREGROUND="#e4e4e4"             # white
+export GUM_FILTER_PROMPT_FOREGROUND="#3AD900"           # green
+export GUM_FILTER_HEADER_FOREGROUND="#FFC600"           # cobalt2 yellow
+export GUM_FILTER_SELECTED_PREFIX_FOREGROUND="#00AAFF"  # cobalt2 blue
+export GUM_FILTER_UNSELECTED_PREFIX_FOREGROUND="#8a8a8a" # light gray
+
+export GUM_CHOOSE_CURSOR_FOREGROUND="#FFC600"           # cobalt2 yellow
+export GUM_CHOOSE_HEADER_FOREGROUND="#FFC600"           # cobalt2 yellow
+export GUM_CHOOSE_SELECTED_FOREGROUND="#00AAFF"         # cobalt2 blue
+
+export GUM_CONFIRM_PROMPT_FOREGROUND="#FFC600"          # cobalt2 yellow
+export GUM_CONFIRM_SELECTED_FOREGROUND="#00AAFF"        # cobalt2 blue
+export GUM_CONFIRM_UNSELECTED_FOREGROUND="#8a8a8a"      # light gray
+
+export GUM_INPUT_PROMPT_FOREGROUND="#FFC600"            # cobalt2 yellow
+export GUM_INPUT_CURSOR_FOREGROUND="#00AAFF"            # cobalt2 blue
+export GUM_INPUT_PLACEHOLDER_FOREGROUND="#8a8a8a"       # light gray
+# -------------------------------------------------------------------
+# }}}
+# -------------------------------------------------------------------
--- zsh/.zsh_plugins_config/init.zsh
+++ zsh/.zsh_plugins_config/init.zsh
@@ -1,3 +1,4 @@
 source $HOME/.zsh_plugins_config/zsh-vi-mode.zsh
 source $HOME/.zsh_plugins_config/fzf.zsh
 source $HOME/.zsh_plugins_config/zoxide.zsh
+source $HOME/.zsh_plugins_config/gum.zsh
