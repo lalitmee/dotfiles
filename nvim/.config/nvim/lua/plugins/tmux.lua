5124f12d958a32694c76c7272cb5e2b08d65ca6e
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-10-30 01:39:44 +0530

    feat(nvim): add npm and yarn tmux keybindings
    - add keybindings for common npm and yarn commands
    - create dedicated which-key groups for discoverability
    - map npm commands under <localleader>tn
    - map yarn commands under <localleader>ty

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -35,6 +35,18 @@ return {
                 questions = { { question = "Package name:", required = true } },
             })
 
+            local npm_start = tmux_manager.run({ cmd = "npm start", name = "NPM Start" })
+            local npm_test = tmux_manager.run({ cmd = "npm test", name = "NPM Test" })
+            local npm_build = tmux_manager.run({ cmd = "npm run build", name = "NPM Build" })
+            local npm_budgets = tmux_manager.run({ cmd = "npm run budgets", name = "NPM Budgets" })
+            local npm_install = tmux_manager.run({ cmd = "npm install", name = "NPM Install" })
+
+            local yarn_start = tmux_manager.run({ cmd = "yarn start", name = "Yarn Start" })
+            local yarn_test = tmux_manager.run({ cmd = "yarn test", name = "Yarn Test" })
+            local yarn_build = tmux_manager.run({ cmd = "yarn run build", name = "Yarn Build" })
+            local yarn_budgets = tmux_manager.run({ cmd = "yarn run budgets", name = "Yarn Budgets" })
+            local yarn_install = tmux_manager.run({ cmd = "yarn install", name = "Yarn Install" })
+
             local wk = require("which-key")
             wk.add({
                 { "<localleader>t", group = "tmux" },
@@ -104,6 +116,80 @@ return {
                         yarn_add()
                     end,
                     desc = "Tmux: Run Yarn Add",
+                },
+
+                { "<localleader>tn", group = "NPM Commands" },
+                {
+                    "<localleader>tnr",
+                    function()
+                        npm_start()
+                    end,
+                    desc = "Tmux: Run NPM Start",
+                },
+                {
+                    "<localleader>tnt",
+                    function()
+                        npm_test()
+                    end,
+                    desc = "Tmux: Run NPM Test",
+                },
+                {
+                    "<localleader>tnb",
+                    function()
+                        npm_build()
+                    end,
+                    desc = "Tmux: Run NPM Build",
+                },
+                {
+                    "<localleader>tnu",
+                    function()
+                        npm_budgets()
+                    end,
+                    desc = "Tmux: Run NPM Budgets",
+                },
+                {
+                    "<localleader>tni",
+                    function()
+                        npm_install()
+                    end,
+                    desc = "Tmux: Run NPM Install",
+                },
+
+                { "<localleader>ty", group = "Yarn Commands" },
+                {
+                    "<localleader>tyr",
+                    function()
+                        yarn_start()
+                    end,
+                    desc = "Tmux: Run Yarn Start",
+                },
+                {
+                    "<localleader>tyt",
+                    function()
+                        yarn_test()
+                    end,
+                    desc = "Tmux: Run Yarn Test",
+                },
+                {
+                    "<localleader>tyb",
+                    function()
+                        yarn_build()
+                    end,
+                    desc = "Tmux: Run Yarn Build",
+                },
+                {
+                    "<localleader>tyu",
+                    function()
+                        yarn_budgets()
+                    end,
+                    desc = "Tmux: Run Yarn Budgets",
+                },
+                {
+                    "<localleader>tyi",
+                    function()
+                        yarn_install()
+                    end,
+                    desc = "Tmux: Run Yarn Install",
                 },
             })
         end,
