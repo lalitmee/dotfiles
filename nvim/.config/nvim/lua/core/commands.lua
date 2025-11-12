3e0446bde8a9bb63aaeb16f3eb76b2011dca5d0e
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-11-13 01:00:04 +0530

    auto: dotfiles backup on 2025-11-13 01:00:04

--- nvim/.config/nvim/lua/core/commands.lua
+++ nvim/.config/nvim/lua/core/commands.lua
@@ -132,4 +132,23 @@ end)
 -- }}}
 --------------------------------------------------------------------------------
 
+--------------
+-- Difftool --
+--------------
+vim.api.nvim_create_user_command("DirDiff", function(opts)
+    if vim.tbl_count(opts.fargs) ~= 2 then
+        vim.notify("DirDiff requires exactly two directory arguments", vim.log.levels.ERROR)
+        return
+    end
+
+    vim.cmd("tabnew")
+    vim.cmd.packadd("nvim.difftool")
+    require("difftool").open(opts.fargs[1], opts.fargs[2], {
+        rename = {
+            detect = false,
+        },
+        ignore = { ".git" },
+    })
+end, { complete = "dir", nargs = "*" })
+
 -- vim:foldmethod=marker
