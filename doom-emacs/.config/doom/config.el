354fc65660710a1f16688b22dc3713ad5e557dae
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-11-21 01:00:04 +0530

    auto: dotfiles backup on 2025-11-21 01:00:04

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -24,6 +24,9 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'normal)
+     doom-variable-pitch-font (font-spec :family "MonoLisa Nerd Font" :size 13))
+;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
