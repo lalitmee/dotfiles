e9be20e27f5c40f201111fe88bd3b591f6fbac56
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2026-05-11 16:00:04 +0530

    auto: dotfiles backup on 2026-05-11 16:00:04

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -24,7 +24,7 @@
        company           ; the ultimate code completion backend
        (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        helm              ; the *other* search engine for love and life
-       ido               ; the other *other* search engine...
+       ;; ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
        vertico           ; the search engine of the future
 
@@ -33,7 +33,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
+       ;; (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
        ligatures         ; ligatures and symbols to make your code pretty again
