e882e6b581cc2f30bfb54a16a5ebd11d6344b956
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2026-06-01 15:00:05 +0530

    auto: dotfiles backup on 2026-06-01 15:00:05

--- nvim/.config/nvim/lua/core/buffers.lua
+++ nvim/.config/nvim/lua/core/buffers.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd({ "BufRead" }, {
     group = id,
     pattern = { "*" },
     callback = function()
-        vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
+        vim.api.nvim_create_autocmd({ "InsertEnter" }, {
             buffer = 0,
             once = true,
             callback = function()
@@ -26,11 +26,7 @@ vim.keymap.set("n", "<leader>bu", function()
     local curbufnr = vim.api.nvim_get_current_buf()
     local buflist = vim.api.nvim_list_bufs()
     for _, bufnr in ipairs(buflist) do
-        if
-            vim.bo[bufnr].buflisted
-            and bufnr ~= curbufnr
-            and (vim.fn.getbufvar(bufnr, "bufpersist") ~= 1)
-        then
+        if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, "bufpersist") ~= 1) then
             vim.cmd("bd " .. tostring(bufnr))
         end
     end
