61f65e16e37bc174ce60a329b0dcea62be4df6d9
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2026-06-24 00:49:36 +0530

    feat(ai): integrate windsurf.vim with custom Ctrl keymaps

--- nvim/.config/nvim/lua/plugins/ai/misc.lua
+++ nvim/.config/nvim/lua/plugins/ai/misc.lua
@@ -236,4 +236,50 @@ return {
         },
         opts = {},
     },
+
+    -- Windsurf AI completion integration
+    {
+        "Exafunction/windsurf.vim",
+        event = "BufEnter",
+        init = function()
+            -- Disable default keybindings so we can map them customly
+            vim.g.codeium_disable_bindings = 1
+        end,
+        config = function()
+            -- Disable by default and enable for specific programming languages
+            vim.g.codeium_filetypes_disabled_by_default = true
+            vim.g.codeium_filetypes = {
+                c = true,
+                cpp = true,
+                gitcommit = true,
+                go = true,
+                javascript = true,
+                javascriptreact = true,
+                lua = true,
+                python = true,
+                rust = true,
+                typescript = true,
+                typescriptreact = true,
+                vim = true,
+                yaml = true,
+            }
+
+            -- Custom keymaps (using Ctrl) to avoid conflicts with Copilot/<Tab>
+            vim.keymap.set("i", "<C-g>", function()
+                return vim.fn["codeium#Accept"]()
+            end, { expr = true, silent = true, desc = "Windsurf: Accept suggestion" })
+
+            vim.keymap.set("i", "<C-;>", function()
+                return vim.fn["codeium#CycleCompletions"](1)
+            end, { expr = true, silent = true, desc = "Windsurf: Next suggestion" })
+
+            vim.keymap.set("i", "<C-,>", function()
+                return vim.fn["codeium#CycleCompletions"](-1)
+            end, { expr = true, silent = true, desc = "Windsurf: Prev suggestion" })
+
+            vim.keymap.set("i", "<C-x>", function()
+                return vim.fn["codeium#Clear"]()
+            end, { expr = true, silent = true, desc = "Windsurf: Clear suggestion" })
+        end,
+    },
 }
