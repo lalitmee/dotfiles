cb24d73bbd4185bf97c5943a93a38555027a5c68
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2026-06-30 00:42:02 +0530

    fix(zsh): make fzf-tab use FZF_DEFAULT_OPTS for themed completion menu
    fzf-tab ignores FZF_DEFAULT_OPTS by default, so the tab-completion menu
    showed default colors instead of the cobalt2 theme from ~/.fzf_config.
    the use-fzf-default-opts zstyle opts it in.

--- zsh/.zsh_plugins_config/zoxide.zsh
+++ zsh/.zsh_plugins_config/zoxide.zsh
@@ -1,6 +1,10 @@
 # -------------------------------------------------------------------
 # NOTE: fzf-tab configuration for zoxide {{{
 # -------------------------------------------------------------------
+# fzf-tab ignores FZF_DEFAULT_OPTS by default; this makes the tab-completion
+# menu pick up our cobalt2 colors/layout from ~/.fzf_config.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+
 # Configure fzf-tab to show current directory contents for z command
 zstyle ':fzf-tab:complete:(cd|z|__zoxide_z):*' fzf-preview 'ls -la $realpath'
 zstyle ':fzf-tab:complete:(cd|z|__zoxide_z):*' fzf-flags '--height=40%'
