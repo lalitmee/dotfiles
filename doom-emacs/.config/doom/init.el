f9a89cc2637b70063ebb738dc10972f5ad4a7b6c
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2026-07-24 04:00:04 +0530

    auto: dotfiles backup on 2026-07-24 04:00:04

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -157,7 +157,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty +journal +brain +roam2 +pomodoro)               ; organize your plain life in plain text
+       (org +pretty +journal +brain +roam +pomodoro)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
